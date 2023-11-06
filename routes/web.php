4c82efa187c4897087b131d92df32902e9045f8a
Miguel <5514787@alu.murciaeduca.es>
2023-11-06 09:47:49 +0100

    ejerciciosRutasHechos

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,39 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Página principal";
 });
+
+Route::get('login', function () {
+    return "Login usuario";
+});
+
+Route::get('logout', function () {
+    return "Logout usuario";
+});
+
+Route::get('proyectos', function () {
+    return "Listado proyectos";
+});
+
+Route::get('proyectos/show/{id?}', function ($id) {
+
+    return "Vista detalle proyecto " . $id;
+
+}) ->where('id', '[0-9]+');
+
+Route::get('create', function () {
+    return "Añadir proyecto";
+});
+
+Route::get('proyectos/edit/{id?}', function ($id) {
+
+    return "Modificar proyecto " . $id;
+
+}) ->where('id', '[0-9]+');
+
+Route::get('perfil/{id?}', function ($id=0) {
+
+    return "Visualizar curriculo de  " . $id;
+
+}) ->where('id', '[0-9]+');
