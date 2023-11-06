fe1f4b7349fbc058f35b14f2bac091a031c12ac9
Alejandro Martínez <11183658@alu.murciaeduca.es>
2023-11-06 19:35:47 +0100

    ejerciciosRutasAlejandro

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Pantalla principal';
 });
+
+Route::get('/login', function () {
+    return 'Login usuario';
+});
+
+Route::get('/logout', function () {
+    return 'Logout usuario';
+});
+
+Route::get('/proyectos', function () {
+    return 'Listado proyectos';
+});
+
+Route::get('/proyectos/show/{id}', function ($id) {
+    return 'Vista detalle proyecto ' . $id;
+})->where('id', '[0-9]+');
+
+Route::get('/proyectos/create', function () {
+    return 'Añadir proyecto';
+});
+
+Route::get('/proyectos/edit/{id}', function ($id) {
+    return 'Modificar proyecto ' . $id;
+})->where('id', '[0-9]+');
+
+
+
