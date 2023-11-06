c043ed443dc619995653d79d575a5661e7cd8aec
Zaidanxp <3720290@alu.murciaeduca.es>
2023-11-06 21:32:11 +0100

    EjerciciosRutas_JF

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use function PHPUnit\Framework\isNull;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +16,41 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Pantalla principal";
+});
+
+Route::get("/login",function(){
+    return "Login usuario";
+});
+
+Route::get("/logout",function(){
+    return "Logout usuario";
+});
+
+Route::prefix("proyectos")->group(function(){
+    Route::get("/",function(){
+        return "Listado proyectos";
+    });
+
+    Route::get("/show/{id}",function($id){
+        return "Vista detalla proyecto $id";
+    })->where("id","[0-9]+");
+
+    Route::get("/create",function(){
+        return "Añadir proyecto";
+    });
+
+    Route::get("/edit/{id}",function($id){
+        return "Modificar proyecto $id";
+    })->where("id","[0-9]+");
+});
+
+Route::get("/perfil/{id?}",function($id){
+    $salida = "Visualizar el currículo";
+    if(is_numeric($id)){
+        $salida = $salida + " de $id";
+    }else{
+        $salida = $salida + " propio";
+    }
+    return $salida;
 });
