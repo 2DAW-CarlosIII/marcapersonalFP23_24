76bb1a7f5059149754c5bfa3b47da2d596d788f3
Sergio Gutiérrez Valverde <1833914@alu.murciaeduca.es>
2023-11-06 09:54:21 +0100

    Ejercicio rutas añadido

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return "Login usuario";
+});
+
+Route::get('/logout', function () {
+    return "Logout usuario";
+});
+
+Route::prefix('/proyectos')->group(function () {
+
+    Route::get('/', function () {
+        return "Listado de proyectos";
+    });
+
+    Route::get('/show/{id}', function ($id) {
+        return "Vista detalle proyecto ". $id;
+    })->where('id', '[0-9]+');
+
+    Route::get('/create', function () {
+        return "Añadir proyecto";
+    });
+
+    Route::get('/edit/{id}', function ($id) {
+        return "Modificar proyecto ". $id;
+    })->where('id', '[0-9]+');
+});
+
+Route::get('/perfil/{id}', function ($id) {
+    return "Visualizar el currículo de ". $id;
+})->where('id', '[0-9]+');
