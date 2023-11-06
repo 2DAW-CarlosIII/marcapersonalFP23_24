bd8d8f630d677fdbfdec275307ffabea21e35016
Honorio Conesa <2918257@alu.murciaeduca.es>
2023-11-06 10:06:38 +0100

    Realizado ejercicio de rutas de clase

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Pantalla principal";
+});
+Route::get('login', function () {
+    return "Login usuario";
+});
+Route::get('logout', function () {
+    return "Logout usuario";
+});
+Route::prefix('proyectos')->group(function () {
+    Route::get('/', function () {
+        return "Listado proyectos";
+    });
+    Route::get('/show/{id}', function ($id) {
+        return "Vista detalle proyecto " . $id;
+    });
+    Route::get('/create', function () {
+        return "Añadir proyecto";
+    });
+    Route::get('/edit/{id}', function ($id) {
+        return "Modificar proyecto " . $id;
+    });
+});
+Route::get('/perfil/{id?}', function ($id = null) {
+    if($id !== null){
+        return "Visualizar el currículo de " . $id;
+    } else {
+        return "Visualizar el currículo propio";
+    }
 });
