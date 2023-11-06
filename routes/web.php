6f4b3ed79547ca74f159cabfe37aadab2a1382be
mdmmr97 <3730156@alu.murciaeduca.es>
2023-11-06 09:53:30 +0100

    Realizado ejercicio de Rutas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,42 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Pantalla principal";
+});
+
+Route::get('login', function(){
+    return "Login usuario";
+});
+
+Route::get('logout', function()
+{
+    return "Logout usuario";
+})
+->where('nombre', '[A-Za-z]+');
+
+Route::get('perfil/{id}', function($id)
+{
+    return "Visualizar el currículo de {" .$id ."}";
+})
+->where('id', '[0-9]+');
+
+Route::prefix('proyectos')->group(function () {
+
+    Route::get('/', function () {
+        return "Listado de proyectos";
+    });
+
+    Route::get('/show/{id}', function ($id) {
+        return "Vista detalle proyecto {" .$id ."}";
+    })
+    ->where('id', '[0-9]+');
+
+    Route::get('/create', function () {
+        return "Añadir proyecto";
+    });
+
+    Route::get('/edit/{id}', function ($id) {
+        return "Modificar proyecto {" .$id ."}";
+    })
+    ->where('id', '[0-9]+');
 });
