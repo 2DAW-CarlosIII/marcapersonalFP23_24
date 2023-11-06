6a2ec17f301e9d0727ee31cd960d599b0c4719ab
Angel De Antonio <1868615@alu.murciaeduca.es>
2023-11-06 19:49:39 +0100

    Ejercicios Rutas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,40 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Pantalla principal';
 });
+
+Route::get('/login', function () {
+    return 'Login usuario';
+});
+
+Route::get('/logout', function () {
+    return 'Logout usuario';
+});
+
+Route::get('/proyectos', function () {
+    return 'Listado proyectos';
+});
+
+Route::get('/proyectos/show/{id}', function ($id) {
+    return 'Vista detalle proyecto' . $id;
+})
+->where('id', '[0-9]+');
+
+Route::get('/proyectos/create', function () {
+    return 'Añadir proyecto';
+});
+
+Route::get('/proyectos/edit/{id}', function ($id) {
+    return 'Modificar proyecto' . $id;
+})
+->where('id', '[0-9]+');
+
+Route::get('/perfil/{id?}', function ($id = null) {
+    if ($id) {
+        return 'Visualizar el currículo de ' . $id;
+    } else {
+        return 'Visualizar el currículo propio';
+    }
+})
+->where('id', '[0-9]+')->name('perfil');
