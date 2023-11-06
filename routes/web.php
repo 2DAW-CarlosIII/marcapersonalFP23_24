fd7cefd6181e36a2638e600ae9ebe08e08494e9d
Jose Manuel Lisón <1173569@alu.murciaeduca.es>
2023-11-06 09:58:11 +0100

    Ejercicio resuelto

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Pantalla principal";
+});
+
+Route::get('login', function () {
+    return "Login usuario";
+});
+
+Route::get('logout', function () {
+    return "Logout usuario";
+});
+
+Route::prefix('proyectos')->group(function () {
+    Route::get('/', function () {
+        return "Listado proyectos";
+    });
+    Route::get('/show/{id}', function ($id) {
+        return "Vista detalle proyecto id:" . $id;
+    });
+    Route::get('/create', function ($id) {
+        return "Añadir proyecto id:" . $id;
+    });
+    Route::get('/edit/{id}', function ($id) {
+        return "Modificar proyecto id:" . $id;
+    });
+
+});
+
+Route::get('perfil/{id}', function ($id) {
+    return "Visualizar el currículo de id:" . $id;
 });
