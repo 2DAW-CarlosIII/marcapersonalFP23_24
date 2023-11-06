0548445543829adebbfb7062a56cc5591200d106
MarianoConesa <1784440@alu.murciaeduca.es>
2023-11-06 09:54:14 +0100

    Ejercicios rutas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hola/{tag?}', function ($tag = "h1") {
+    echo "<" . $tag . ">it works</". $tag .">";
+});
+*/
+
+Route::get('/', function () {
+    return "Pantalla principal";
+});
+
+Route::get('/login', function () {
+    return "Login Usuario";
+});
+
+Route::get('/logout', function () {
+    return "Logout Usuario";
+});
+
+Route::get('/proyectos', function () {
+    return "Login Usuario";
+});
+
+Route::get('/proyectos', function () {
+    return "Listado de proyectos";
+});
+
+Route::get('/proyectos/show/{id}', function ($id) {
+    return "Vista a detalle de proyecto " . $id;
+})->where('id', '[0-9]+');
+
+Route::get('/proyectos/create', function () {
+    return "Añadir proyecto";
+});
+
+Route::get('/proyectos/edit/{id}', function ($id) {
+    return "Modificar proyecto " . $id;
+})->where('id', '[0-9]+');
+
+Route::get('/perfil/{id?}', function ($id = null) {
+    $result = $id ? "Visualizar el currículo de " . $id : "Visualizar curriculo propio";
+    return $result;
+})->where('id', '[0-9]+');
+
+
