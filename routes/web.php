2e634566686ddc7fd34fcb21028a8302bd578847
Antonio Amate <4252008@alu.murciaeduca.es>
2023-11-07 08:26:11 +0100

    Created static content in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,56 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Pantalla principal
 Route::get('/', function () {
-    return view('welcome');
+    return "Pantalla principal";
+});
+
+// Login usuario
+Route::get('/login', function () {
+    return "Login usuario";
+});
+
+// Logout usuario
+Route::get('/logout', function () {
+    return "Logout usuario";
+});
+
+// Listado proyectos
+Route::get('/proyectos', function () {
+    return "Listado proyectos";
+});
+
+// Vista detalle proyecto {id}
+Route::get('/proyectos/show/{id}', function ($id) {
+    if (is_numeric($id) && $id > 0) {
+        return "Vista detalle proyecto $id";
+    } else {
+        abort(404);
+    }
+});
+
+// Añadir proyecto
+Route::get('/proyectos/create', function () {
+    return "Añadir proyecto";
+});
+
+// Modificar proyecto {id}
+Route::get('/proyectos/edit/{id}', function ($id) {
+    if (is_numeric($id) && $id > 0) {
+        return "Modificar proyecto $id";
+    } else {
+        abort(404);
+    }
+});
+
+// Visualizar el currículo
+Route::get('/perfil/{id?}', function ($id = null) {
+    if ($id && is_numeric($id) && $id > 0) {
+        return "Visualizar el currículo de $id";
+    } elseif ($id === null) {
+        return "Visualizar el currículo propio";
+    } else {
+        abort(404);
+    }
 });
