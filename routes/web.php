1b20af600d24fc119b3bd140ad347cd979019885
Andrea Escudero <7460086@alu.murciaeduca.es>
2023-11-07 09:51:42 +0100

    ejercicios de rutas resueltos

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,40 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Pantalla principal';
+});
+
+Route::get('/login', function()
+{
+    return 'Login usuario';
+});
+
+Route::get('logout', function()
+{
+    return 'Logout usuario';
+});
+
+Route::get('proyectos', function()
+{
+    return 'Listado proyectos';
+});
+
+Route::get('/proyectos/show/{id}', function ($id) {
+
+    return 'Vista detalle proyecto ' . $id;
+});
+
+Route::get('proyectos/create', function()
+{
+    return 'Añadir proyecto';
+});
+
+Route::get('/proyectos/edit/{id}', function ($id) {
+
+    return 'Modificar proyecto ' . $id;
+});
+
+Route::get('/perfil/{id}', function ($id) {
+
+    return 'Visualizar el currículo de ' . $id;
 });
