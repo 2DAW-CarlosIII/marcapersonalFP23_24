1f55dc37374fc2ec012af2a23b7195857798020f
MarianoConesa <1784440@alu.murciaeduca.es>
2023-11-09 08:07:40 +0100

    ejs vistas

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Mi Web</title>
+    </head>
+    <body>
+        <h1>¡Hola {{$nombre}}!</h1>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,29 +17,38 @@ Route::get('/', function () {
     return "Pantalla principal";
 });
 
+Route::get('home/{nombre?}', function ($nombre = 'Jhon Doe') {
+    return view('home', array('nombre' => $nombre));
+});
+
+/*Route::get('home/{nombre?}', function ($nombre = 'Jhon Doe') {
+    $vista = view('home') ->with('nombre',$nombre);
+    return $vista;
+});*/
+
 Route::get('login', function () {
-    return "Login usuario";
+    return view("auth.login");
 });
 
 Route::get('logout', function () {
-    return "Logout usuario";
+    return view("auth.login");
 });
 
 Route::prefix('proyectos')->group(function () {
     Route::get('/', function () {
-        return "Listado proyectos";
+        return view("proyectos.index");
     });
     Route::get('/show/{id}', function ($id) {
-        return "Vista detalle proyecto " . $id;
+        return view('proyectos.show') ->with('id',$id);
     })
-        ->where('id', '[0-9]+');
+     ->where('id', '[0-9]+');
 
     Route::get('/create', function () {
-        return "Añadir proyecto";
+        return view('proyectos.create');
     });
 
     Route::get('/edit/{id}', function ($id) {
-        return "Modificar proyecto " . $id;
+        return view('proyectos.edit') -> with('id', $id);
     })
         ->where('id', '[0-9]+');
 });
