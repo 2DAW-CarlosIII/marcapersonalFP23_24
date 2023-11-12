2b74fbb6127ab3d6ebf46b1579f4a02cad7bbd5a
Zaidanxp <3720290@alu.murciaeduca.es>
2023-11-12 16:08:13 +0100

    Creación de nuevas vistas a la página marcapersonal enlazas con sus repectivas rutas

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('login', function () {
-    return "Login usuario";
+    return view('auth.login');
 });
 
 Route::get('logout', function () {
@@ -27,19 +27,19 @@ Route::get('logout', function () {
 
 Route::prefix('catalog')->group(function () {
     Route::get('/', function () {
-        return "Listado proyectos";
+        return view('catalog.index');
     });
     Route::get('/show/{id}', function ($id) {
-        return "Vista detalle proyecto " . $id;
+        return view('catalog.show',array("id"=>$id));
     })
         ->where('id', '[0-9]+');
 
     Route::get('/create', function () {
-        return "Añadir proyecto";
+        return view('catalog.create');
     });
 
     Route::get('/edit/{id}', function ($id) {
-        return "Modificar proyecto " . $id;
+        return view('catalog.edit',array("id"=>$id));
     })
         ->where('id', '[0-9]+');
 });
@@ -50,5 +50,4 @@ Route::get('perfil/{id?}', function ($id = null) {
     } else {
         return "Visualizar el currículo de " . $id;
     }
-})
-    ->where('id', '[0-9]+');
+})->where('id', '[0-9]+');
