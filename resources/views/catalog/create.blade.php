4fd44c0482e143577c30d1051deecfe9faad1f4c
Andrea <7460086@alu.murciaeduca.es>
2023-11-12 23:31:19 +0100

    ejercicios pasados por el test correctamente

---
+++ resources/views/catalog/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+
+    Añadir proyecto
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -14,35 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "Pantalla principal";
+    return view ('home');
 });
 
 Route::get('login', function () {
-    return "Login usuario";
+    return view ('auth.login');
 });
 
 Route::get('logout', function () {
     return "Logout usuario";
 });
 
-Route::prefix('proyectos')->group(function () {
-    Route::get('/', function () {
-        return "Listado proyectos";
-    });
-    Route::get('/show/{id}', function ($id) {
-        return "Vista detalle proyecto " . $id;
-    })
-        ->where('id', '[0-9]+');
-
-    Route::get('/create', function () {
-        return "Añadir proyecto";
-    });
-
-    Route::get('/edit/{id}', function ($id) {
-        return "Modificar proyecto " . $id;
-    })
-        ->where('id', '[0-9]+');
+Route::get('catalog', function () {
+    return view ('catalog.index');
 });
+
+Route::get('catalog/show/{id}', function ($id) {
+    return view('catalog.show', array('id'=>$id));
+}) ->where('id', '[0-9]+');
+
+Route::get('catalog/create', function () {
+    return view ('catalog.create');
+});
+
+Route::get('catalog/edit/{id}', function ($id) {
+    return view ('catalog.edit', array('id'=>$id));
+}) ->where('id', '[0-9]+');
 
 Route::get('perfil/{id?}', function ($id = null) {
     if ($id == null) {
