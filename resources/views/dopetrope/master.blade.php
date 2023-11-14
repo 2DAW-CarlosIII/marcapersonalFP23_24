bb0769fa6e332d42a33396178d6ffb318540460f
Manuel Fernandez <1808965@alu.murciaeduca.es>
2023-11-14 08:49:47 +0100

    Hasta la incorporación del navbar

---
+++ resources/views/dopetrope/master.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<!--
+ Dopetrope by HTML5 UP
+ html5up.net | @ajlkn
+ Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+
+<head>
+    <title>Dopetrope by HTML5 UP</title>
+    <meta charset="utf-8" />
+    <meta name="visewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="{{ asset('/dopetrope/assets/css/main.css') }}" />
+</head>
+
+<body class="homepage is-preload">
+    <div id="page-wrapper">
+
+        <!-- Header -->
+        @include('dopetrope.partials.header')
+
+        <!-- Main -->
+        @include('dopetrope.partials.main')
+
+        <!-- Footer -->
+        @include('dopetrope.partials.footer')
+
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('/dopetrope/assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('/dopetrope/assets/js/jquery.dropotron.min.js') }}"></script>
+    <script src="{{ asset('/dopetrope/assets/js/browser.min.js') }}"></script>
+    <script src="{{ asset('/dopetrope/assets/js/breakpoints.min.js') }}"></script>
+    <script src="{{ asset('/dopetrope/assets/js/util.js') }}"></script>
+    <script src="{{ asset('/dopetrope/assets/js/main.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
