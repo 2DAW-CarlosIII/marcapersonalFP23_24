db9e1977f8c0aee569b78281e3fb965152b182ef
Jesus <4186790@alu.murciaeduca.es>
2023-11-14 20:24:10 +0100

    Se ha quitado la coma en la dirección, el -f en facebook pero NO FUNCIONAN los colores en el css

--- resources/views/dopetrope/partials/footer/c3info.blade.php
+++ resources/views/dopetrope/partials/footer/c3info.blade.php
@@ -3,7 +3,7 @@
         <h2>C.I.F.P. Carlos III</h2>
     </header>
     <ul class="social">
-        <li><a class="icon brands fa-facebook-f" href="https://www.facebook.com/cifpcarlos3/?locale=es_ES"><span class="label">Facebook</span></a></li>
+        <li><a class="icon brands fa-facebook" href="https://www.facebook.com/cifpcarlos3/?locale=es_ES"><span class="label">Facebook</span></a></li>
         <li><a class="icon brands fa-twitter" href="https://twitter.com/cifpcarlos3"><span class="label">Twitter</span></a></li>
         <li><a class="icon brands fa-instagram" href="https://www.instagram.com/explore/locations/468129984/cifp-carlos-iii/"><span class="label">Instagram</span></a></li>
         <li><a class="icon brands fa-tiktok" href="https://www.tiktok.com/@cifpcarlos3"><span class="label">Tiktok</span></a></li>
@@ -12,7 +12,7 @@
         <li>
             <h3>Dirección</h3>
             <p>
-                C/Carlos III, 3, <br>Cartagena 30201
+                C/Carlos III, 3 <br>Cartagena 30201
             </p>
         </li>
         <li>
