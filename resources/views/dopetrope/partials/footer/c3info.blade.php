d9118c8cf38b73b1c5d0fa9355cee86b5f575700
Jesus <4186790@alu.murciaeduca.es>
2023-11-14 14:31:11 +0100

    archivo creado y terminado

---
+++ resources/views/dopetrope/partials/footer/c3info.blade.php
@@ -0,0 +1,27 @@
+<section>
+    <header>
+        <h2>C.I.F.P. Carlos III</h2>
+    </header>
+    <ul class="social">
+        <li><a class="icon brands fa-facebook-f" href="https://www.facebook.com/cifpcarlos3/?locale=es_ES"><span class="label">Facebook</span></a></li>
+        <li><a class="icon brands fa-twitter" href="https://twitter.com/cifpcarlos3"><span class="label">Twitter</span></a></li>
+        <li><a class="icon brands fa-instagram" href="https://www.instagram.com/explore/locations/468129984/cifp-carlos-iii/"><span class="label">Instagram</span></a></li>
+        <li><a class="icon brands fa-tiktok" href="https://www.tiktok.com/@cifpcarlos3"><span class="label">Tumblr</span></a></li>
+    </ul>
+    <ul class="contact">
+        <li>
+            <h3>Dirección</h3>
+            <p>
+                C/Carlos III, 3, <br>Cartagena 30201
+            </p>
+        </li>
+        <li>
+            <h3>Correo</h3>
+            <p><a href="mailto:30019702@murciaeduca.es">30019702@murciaeduca.es</a></p>
+        </li>
+        <li>
+            <h3>Teléfono</h3>
+            <p>968 321301</p>
+        </li>
+    </ul>
+</section>
