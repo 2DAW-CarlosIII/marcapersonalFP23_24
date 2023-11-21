57ab5d8e018bb17222f78140a05d843490acef98
Luis Aviles <1812123@alu.murciaeduca.es>
2023-11-21 09:54:58 +0100

    Realizados ejercicios Controladores

--- resources/views/catalog/edit.blade.php
+++ resources/views/catalog/edit.blade.php
@@ -1,7 +1,56 @@
 @extends('layouts.master')
 
+
 @section('content')
 
-    Modificar proyecto {{$id}}
+<div class="row" style="margin-top:40px">
+   <div class="offset-md-3 col-md-6">
+      <div class="card">
+         <div class="card-header text-center">
+            Modificar proyecto
+         </div>
+         <div class="card-body" style="padding:30px">
 
-@stop
+            <form action="{{ action([App\Http\Controllers\CatalogController::class, 'putEdit'], ['id' => $id]) }}" method="POST">
+                @method('PUT')
+	            @csrf
+
+	            <div class="form-group">
+	               <label for="nombre">Nombre</label>
+	               <input type="text" name="nombre" id="nombre" value="{{$proyecto['nombre']}}" class="form-control">
+	            </div>
+
+	            <div class="form-group">
+	            	<label for="docente_id">Docente</label>
+	               <input type="number" name="docente_id" id="docente_id" value="{{$proyecto['docente_id']}}">
+	            </div>
+
+	            <div class="form-group">
+	            	<label for="dominio">Dominio</label><br/>
+                    https://github.com/2DAW-CarlosIII/
+	               <input type="text" name="dominio" id="dominio" value="{{$proyecto['dominio']}}" class="form-control">
+	            </div>
+
+	            <div class="form-group">
+	               <label for="metadatos">Metadatos</label>
+	               <textarea name="metadatos" id="metadatos" class="form-control" rows="3">
+{{print_r($proyecto['metadatos'], true)}}
+                   </textarea>
+                   <br /><small>Cada metadato irá separado del siguiente por una línea <br />
+                   y la clave irá separada por : del valor</small>
+	            </div>
+
+	            <div class="form-group text-center">
+	               <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+	                   Modificar proyecto
+	               </button>
+	            </div>
+
+            </form>
+
+         </div>
+      </div>
+   </div>
+</div>
+
+@endsection
