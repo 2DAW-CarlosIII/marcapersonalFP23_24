2b55fade717c603a0d98c8865da2cb9c69e46f73
mdmmr97 <3730156@alu.murciaeduca.es>
2023-11-22 21:50:15 +0100

    Issues de UserController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\CatalogController;
 use App\Http\Controllers\HomeController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -36,6 +38,19 @@ Route::prefix('catalog')->group(function () {
     Route::put('/edit/{id}', [CatalogController::class, 'putEdit'])->where('id', '[0-9]+');
 
     Route::get('/edit/{id}', [CatalogController::class, 'getEdit'])->where('id', '[0-9]+');
+});
+
+Route::prefix('users')->group(function () {
+
+    Route::get('/', [UserController::class, 'getIndex']);
+
+    Route::get('/show/{id}', [UserController::class, 'getShow'])->where('id', '[0-9]+');
+
+    Route::get('/create', [UserController::class, 'getCreate']);
+
+    Route::put('/edit/{id}', [UserController::class, 'putEdit'])->where('id', '[0-9]+');
+
+    Route::get('/edit/{id}', [UserController::class, 'getEdit'])->where('id', '[0-9]+');
 });
 
 Route::get('perfil/{id?}', function ($id = null) {
---
+++ tests/Feature/UserControllerTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        /**
+         * proyectos index test.
+         */
+        $response = $this->get('/users');
+        $first_name = [
+            'John',
+            'Jane',
+            'Alice',
+            'Bob',
+            'Eva',
+            'Michael',
+            'Sophie',
+            'David',
+            'Emily',
+            'Tom',
+        ];
+
+        $response
+        ->assertStatus(200)
+        ->assertViewIs('users.index')
+        ->assertSeeTextInOrder($first_name, $escaped = true);
+
+    /**
+     * proyectos show test.
+     */
+        $response = $this->get("/users/show/1");
+
+        $response
+        ->assertStatus(200)
+        ->assertViewIs('users.show')
+        ->assertSeeText('Jane', $escaped = true);
+
+        $response = $this->get("/users/show/2");
+
+        $response
+        ->assertSeeText('Alice', $escaped = true);
+
+        $response = $this->get("/users/show/A");
+        $response->assertNotFound();
+
+    /**
+     * proyectos create test.
+     */
+        $value = 'Añadir usuario';
+        $response = $this->get('/users/create');
+
+        $response
+        ->assertStatus(200)
+        ->assertViewIs('users.create')
+        ->assertSeeText($value, $escaped = true);
+
+    /**
+     * proyectos edit test.
+     */
+        $id = rand(1, 10);
+        $value = "Modificar usuario";
+        $response = $this->get("/users/edit/$id");
+
+        $response
+        ->assertStatus(200)
+        ->assertViewIs('users.edit')
+        ->assertSeeText($value, $escaped = true);
+
+        $response = $this->get("/users/edit/A");
+        $response->assertNotFound();
+
+
+
+    }
+}
