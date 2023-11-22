6704faf56b5f54728b3c087eeee852ae2f359b37
PedroDavid <4103404@alu.murciaeduca.es>
2023-11-22 08:58:35 +0100

    ejercicios con los test pasados

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CatalogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'getHome']);
 
 Route::get('login', function () {
     return view('auth.login');
@@ -26,20 +26,20 @@ Route::get('logout', function () {
 });
 
 Route::prefix('catalog')->group(function () {
-    Route::get('/', function () {
-        return view('catalog.index');
-    });
-    Route::get('/show/{id}', function ($id) {
-        return view('catalog.show', array('id' => $id));
-    })->where('id', '[0-9]+');
+    Route::get('/', [CatalogController::class, 'getIndex']);
+
+    Route::get('/show/{id}', [CatalogController::class, 'getShow'])
+        ->where('id', '[0-9]+');
 
     Route::get('/create', function () {
         return view('catalog.create');
     });
 
-    Route::get('/edit/{id}', function ($id) {
-        return view('catalog.edit', array('id' => $id));
-    })->where('id', '[0-9]+');
+    Route::get('/edit/{id}', [CatalogController::class, 'getEdit'])
+        ->where('id', '[0-9]+');
+
+    Route::put('/edit/{id}', [CatalogController::class, 'putEdit'])
+        ->where('id', '[0-9]+');
 });
 
 Route::get('perfil/{id?}', function ($id = null) {
