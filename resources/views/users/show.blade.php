e03d1353955b97c341019f0127f0313405d33c2e
Alejandro Martínez <11183658@alu.murciaeduca.es>
2023-11-23 10:13:48 +0100

    Ejercicio Controlador UserControllerv2

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,39 +1,35 @@
 @extends('layouts.master')
 
 @section('content')
-
-
     <div class="row m-4">
 
         <div class="col-sm-4">
 
-            <section class="box">
-                <a href="#" class="image featured" title="Nice and Serious, CC0, via Wikimedia Commons">
-                    <img width="256" alt="User (89041) - The Noun Project" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/User_%2889041%29_-_The_Noun_Project.svg/256px-User_%2889041%29_-_The_Noun_Project.svg.png"></a>
-            </section>
+            <a href="#" class="image featured" title="Nice and Serious, CC0, via Wikimedia Commons">
+                <img width="256" alt="User (89041) - The Noun Project"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/User_%2889041%29_-_The_Noun_Project.svg/256px-User_%2889041%29_-_The_Noun_Project.svg.png"></a>
         </div>
-    </section>
-</div>
-<div class="col-8 col-8-medium col-8-small">
-    <section class="box">
-        <header>
-            <h3 class='text-left' >{{ $user['first_name'] }} {{ $user['last_name'] }}</h3>
-        </header>
 
-        <p class="text-dark">linkedin:  {{ $user['linkedin']}}</p>
+        <div class="col-8 col-8-medium col-8-small">
 
-        <footer>
-            <ul class="actions">
-                <li><a href="{{ action([App\Http\Controllers\UserController::class, 'getEdit'], ['id' => $id]) }}"
-                        class="button alt">Editar Usuario</a></li>
-                <li>
-                    <a href="{{ action([App\Http\Controllers\UserController::class, 'getIndex']) }}"
-                        class="button alt">Listar Usuarios</a>
-                </li>
-            </ul>
-        </footer>
-    </section>
-</div>
-</div>
+            <h3 class='text-left'>{{ $user['first_name'] }} {{ $user['last_name'] }}</h3>
 
+            <p class="text-dark">Linkedin: {{ $user['linkedin'] }}<br>
+                Email: {{ $user['linkedin'] }}</p>
+
+
+
+            <footer>
+                <ul class="actions">
+                    <li><a href="{{ action([App\Http\Controllers\UserController::class, 'getEdit'], ['id' => $id]) }}"
+                            class="button alt">Editar Usuario</a></li>
+                    <li>
+                        <a href="{{ action([App\Http\Controllers\UserController::class, 'getIndex']) }}"
+                            class="button alt">Listar Usuarios</a>
+                    </li>
+                </ul>
+            </footer>
+
+        </div>
+    </div>
 @endsection
