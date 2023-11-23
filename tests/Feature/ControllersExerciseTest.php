80dc3acc06b809ab3fe50b2d0e24dcc6d320fcc1
Luis Aviles <1812123@alu.murciaeduca.es>
2023-11-23 10:00:13 +0100

    CurriculoController versionFinal

--- tests/Feature/ControllersExerciseTest.php
+++ tests/Feature/ControllersExerciseTest.php
@@ -39,69 +39,69 @@ class ControllersExerciseTest extends TestCase
             $response->assertStatus(200)->assertSeeText($value, $escaped = true);
 
         /**
-         * curriculos index test.
+         * proyectos index test.
          */
-            $response = $this->get('/curriculos');
-            $curriculos = [
-                'https://www.youtube.com/watch?v=u54uern',
-                'https://www.youtube.com/watch?v=v87dfg2',
-                'https://www.youtube.com/watch?v=frt32qe',
-                'https://www.youtube.com/watch?v=wtrh2we',
-                'https://www.youtube.com/watch?v=qwer123',
-                'https://www.youtube.com/watch?v=ytgfd32',
-                'https://www.youtube.com/watch?v=zxvbn23',
-                'https://www.youtube.com/watch?v=asdf456',
-                'https://www.youtube.com/watch?v=qwerty78',
-                'https://www.youtube.com/watch?v=mnbvc90',
+            $response = $this->get('/catalog');
+            $nombres = [
+                'Tecnologías de la Información',
+                'Diseño Gráfico',
+                'Electrónica',
+                'Ingeniería Civil',
+                'Gastronomía',
+                'Medicina',
+                'Mecatrónica',
+                'Arquitectura',
+                'Automoción',
+                'Turismo',
             ];
 
             $response
             ->assertStatus(200)
-            ->assertViewIs('curriculos.index')
-            ->assertSeeTextInOrder($curriculos, $escaped = true);
+            ->assertViewIs('catalog.index')
+            ->assertSeeTextInOrder($nombres, $escaped = true);
 
         /**
          * proyectos show test.
          */
-            $response = $this->get("/curriculos/show/1");
+            $response = $this->get("/catalog/show/1");
 
             $response
             ->assertStatus(200)
-            ->assertViewIs('curriculos.show')
-            ->assertSeeText('https://www.youtube.com/watch?v=v87dfg2', $escaped = true);
+            ->assertViewIs('catalog.show')
+            ->assertSeeText('Diseño Gráfico', $escaped = true);
 
-            $response = $this->get("/curriculos/show/2");
+            $response = $this->get("/catalog/show/2");
 
             $response
-            ->assertSeeText('https://www.youtube.com/watch?v=frt32qe', $escaped = true);
+            ->assertSeeText('Electrónica', $escaped = true);
 
-            $response = $this->get("/curriculos/show/A");
+            $response = $this->get("/catalog/show/A");
             $response->assertNotFound();
 
         /**
          * proyectos create test.
          */
-            $value = 'Añadir Currículum';
-            $response = $this->get('/curriculos/create');
+            $value = 'Añadir proyecto';
+            $response = $this->get('/catalog/create');
 
             $response
             ->assertStatus(200)
-            ->assertViewIs('curriculos.create')
+            ->assertViewIs('catalog.create')
             ->assertSeeText($value, $escaped = true);
 
         /**
          * proyectos edit test.
          */
             $id = rand(1, 10);
-            $value = "Modificar Currículum";
-            $response = $this->get("/curriculos/edit/$id");
+            $value = "Modificar proyecto";
+            $response = $this->get("/catalog/edit/$id");
 
             $response
             ->assertStatus(200)
-            ->assertViewIs('curriculos.edit')
+            ->assertViewIs('catalog.edit')
             ->assertSeeText($value, $escaped = true);
 
-            $response = $this->get("/curriculos/edit/A");
+            $response = $this->get("/catalog/edit/A");
             $response->assertNotFound();
 
         /**
