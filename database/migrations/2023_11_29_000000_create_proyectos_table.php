cfa6ea41773c34c7dbebdcbee95b928e0a5c1a2a
Jose7420 <7962446@alu.murciaeduca.es>
2023-11-30 21:27:04 +0100

    Ejercicio Base de datos 4_7

---
+++ database/migrations/2023_11_29_000000_create_proyectos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('docente_id')->nullable();
+            $table->string('nombre',120);
+            $table->string('dominio',30)->nullable();
+            $table->text('metadatos')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proyectos');
+    }
+};
