bef84575b60fd3e5dd3bba5a1cfc8c8823d0ff38
mdmmr97 <3730156@alu.murciaeduca.es>
2023-11-30 09:32:45 +0100

    Empezamos ejercicio 47

---
+++ database/migrations/2023_11_29_000000_create_proyectos_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('docente_id')->nullable();
+            $table->string('nombre', 120);
+            $table->string('dominio', 30);
+            $table->textarea('metadatos')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proyectos');
+    }
+};
+
+/*
+public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+*/
