3330e8bc74624c2a82608f541db5e69e5cca27b3
Manuel Fernandez <1808965@alu.murciaeduca.es>
2023-11-30 18:47:40 +0100

    Ejercicios 47 Bases de Datos

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -12,22 +12,22 @@
         </div>
         <div class="col-sm-8">
 
-            <h3><strong>Nombre: </strong>{{ $proyecto['nombre'] }}</h3>
+            <h3><strong>Nombre: </strong>{{ $proyecto->nombre }}</h3>
             <h4><strong>Dominio: </strong>
-                <a href="http://github.com/2DAW-CarlosIII/{{ $proyecto['dominio'] }}">
-                    http://github.com/2DAW-CarlosIII/{{ $proyecto['dominio'] }}
+                <a href="http://github.com/2DAW-CarlosIII/{{ $proyecto->dominio }}">
+                    http://github.com/2DAW-CarlosIII/{{ $proyecto->dominio }}
                 </a>
             </h4>
-            <h4><strong>Docente: </strong>{{ $proyecto['docente_id'] }}</h4>
+            <h4><strong>Docente: </strong>{{ $proyecto->docente_id }}</h4>
             <p><strong>Metadatos: </strong>
                 <ul>
-                    @foreach ($proyecto['metadatos'] as $indice => $metadato)
+                    @foreach ($proyecto->metadatos as $indice => $metadato)
                         <li>{{ $indice }}: {{ $metadato }}</li>
                     @endforeach
                 </ul>
             </p>
             <p><strong>Estado: </strong>
-                @if($proyecto['metadatos']['calificacion'] >= 5)
+                @if($proyecto->metadatos['calificacion'] >= 5)
                     Proyecto aprobado
                 @else
                     Proyecto suspenso
@@ -39,7 +39,7 @@
             @else
                 <a class="btn btn-primary" href="#">Aprobar proyecto</a>
             @endif
-            <a class="btn btn-warning" href="{{ action([App\Http\Controllers\CatalogController::class, 'getEdit'], ['id' => $id]) }}">
+            <a class="btn btn-warning" href="{{ action([App\Http\Controllers\CatalogController::class, 'getEdit'], ['id' => $proyecto->id]) }}">
                 <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
                 Editar proyecto
             </a>
