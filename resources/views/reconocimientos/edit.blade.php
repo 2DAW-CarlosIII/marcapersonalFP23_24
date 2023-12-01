9eef4fe432cab462f564086dec1d4466e4ee9705
Jesus <4186790@alu.murciaeduca.es>
2023-12-01 20:24:28 +0100

    Terminado

--- resources/views/reconocimientos/edit.blade.php
+++ resources/views/reconocimientos/edit.blade.php
@@ -10,33 +10,33 @@
          </div>
          <div class="card-body" style="padding:30px">
 
-            <form action="{{action([App\Http\Controllers\ReconocimientoController::class, 'getEdit'], ['id' => $id])}}" method="POST">
+            <form action="{{action([App\Http\Controllers\ReconocimientoController::class, 'putEdit'], ['id' => $id])}}" method="POST">
                 @method('PUT')
 	            @csrf
 
 	            <div class="form-group">
 	               <label for="estudiante_id">ID estudiante</label>
-	               <input type="number" name="estudiante_id" id="estudiante_id" class="form-control" value="{{ $reconocimiento['estudiante_id'] }}">
+	               <input type="number" name="estudiante_id" id="estudiante_id" class="form-control" value="{{ $reconocimiento->estudiante_id }}">
 	            </div>
 
 	            <div class="form-group">
 	            	<label for="actividad_id">Actividad</label>
-	               <input type="number" name="actividad_id" id="actividad_id" class="form-control" value="{{ $reconocimiento['actividad_id'] }}">
+	               <input type="number" name="actividad_id" id="actividad_id" class="form-control" value="{{ $reconocimiento->actividad_id }}">
 	            </div>
 
 	            <div class="form-group">
 	            	<label for="documento">URL del documento</label>
-	               <input type="url" name="documento" id="documento" class="form-control" value="{{ $reconocimiento['documento'] }}">
+	               <input type="url" name="documento" id="documento" class="form-control" value="{{ $reconocimiento->documento }}">
 	            </div>
 
 	            <div class="form-group">
 	               <label for="fecha">Fecha</label>
-	               <input type ="date" name="fecha" id="fecha" class="form-control">
+	               <input type ="date" name="fecha" id="fecha" class="form-control" value={{ $reconocimiento->created_at}}>
 	            </div>
 
                 <div class="form-group">
 	            	<label for="docente_validador">Docente Validador</label>
-	               <input type="number" name="docente_validador" id="docente_validador" class="form-control" value="{{ $reconocimiento['docente_validador'] }}">
+	               <input type="number" name="docente_validador" id="docente_validador" class="form-control" value="{{ $reconocimiento->docente_validador }}">
 	            </div>
 
 	            <div class="form-group text-center">
