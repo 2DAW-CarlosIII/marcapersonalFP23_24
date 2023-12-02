a7c8f8e1839d7c7d9fc3237c36a6d492b31616b7
Angel De Antonio <1868615@alu.murciaeduca.es>
2023-12-02 16:45:29 +0100

    Issue 4.7.1 Ejercicios de Bases de Datos de Docentes

---
+++ database/seeders/DocentesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Docente;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+
+class DocentesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Docente::truncate();
+        \App\Models\Docente::factory(10)->create();
+    }
+}
