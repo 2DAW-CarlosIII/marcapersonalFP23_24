486915bac32127a82e0b09bbd8efd65cc817c85a
Andrea Escudero <7460086@alu.murciaeduca.es>
2023-12-02 17:55:55 +0100

    solucionados errores de id

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -7,38 +7,41 @@ use Illuminate\Http\Request;
 
 class CatalogController extends Controller
 {
-    public function getIndex(){
-        $proyecto=Proyecto::all();
-        return view('catalog.index',['arrayProyectos'=>$proyecto]);
+    public function getIndex()
+    {
+        $proyecto = Proyecto::all();
+        return view('catalog.index', ['arrayProyectos' => $proyecto]);
     }
 
     public function getShow($id)
     {
-        $proyecto=Proyecto::FindOrFail($id);
+        $proyecto = Proyecto::FindOrFail($id);
         $proyecto->metadatos = unserialize($proyecto->metadatos);
         return view('catalog.show')
             ->with('proyecto', $proyecto)
-            ->with('id', $id);
+            ->with('id', $proyecto->id);
     }
 
-    public function putEdit($id) {
-        $proyecto=Proyecto::FindOrFail($id+1);
+    public function putEdit($id)
+    {
+        $proyecto = Proyecto::FindOrFail($id);
         $proyecto->metadatos = unserialize($proyecto->metadatos);
         return view('catalog.edit')
-            ->with("proyecto",$proyecto)
-            ->with("id",$id);
+            ->with('proyecto', $proyecto)
+            ->with('id', $proyecto->id);
     }
 
-    public function getEdit($id) {
-        $proyecto=Proyecto::FindOrFail($id+1);
+    public function getEdit($id)
+    {
+        $proyecto = Proyecto::FindOrFail($id);
         $proyecto->metadatos = unserialize($proyecto->metadatos);
         return view('catalog.edit')
-            ->with("proyecto",$proyecto)
-            ->with("id",$id);
+            ->with('proyecto', $proyecto)
+            ->with('id', $proyecto->id);
     }
 
-    public function getCreate(){
+    public function getCreate()
+    {
         return view('catalog.create');
     }
-
 }
