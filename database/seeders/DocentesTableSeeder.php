2807e340e78577e08cc33bed7bd8d2faed851ce1
Zaidanxp <3720290@alu.murciaeduca.es>
2023-12-02 10:57:32 +0100

    ISSUE-4.7.1-Componentes para la gestion de datos de docentes en BD

---
+++ database/seeders/DocentesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Docente;
+use Illuminate\Database\Seeder;
+
+class DocentesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Docente::truncate();
+        Docente::factory(10);
+    }
+}
