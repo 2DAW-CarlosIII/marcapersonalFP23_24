c9790aba52bc073d1d3207e0c80301a636323b36
ArturoGomez <1729185@alu.murciaeduca.es>
2023-12-02 14:46:33 +0100

    Ejercicios 4.7

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -10,17 +10,17 @@
         <section class="box">
             <a href="#" class="image featured"><img src="{{ asset('/images/mp-logo.png') }}" alt="" /></a>
             <header>
-                <h3>{{ $arrayProyectos[$i]['nombre'] }}</h3>
+                <h3>{{ $arrayProyectos[$i]->nombre}}</h3>
             </header>
             <p>
-                <a href="http://github.com/2DAW-CarlosIII/{{ $arrayProyectos[$i]['dominio'] }}">
-                    http://github.com/2DAW-CarlosIII/{{ $arrayProyectos[$i]['dominio'] }}
+                <a href="http://github.com/2DAW-CarlosIII/{{ $arrayProyectos[$i]->dominio }}">
+                    http://github.com/2DAW-CarlosIII/{{ $arrayProyectos[$i]->dominio }}
                 </a>
             </p>
             <footer>
                 <ul class="actions">
-                    //TODO enlazar el show de catalogController
-                    <li><a href="{{ action([App\Http\Controllers\CatalogController::class, 'getShow'], ['id' => $i] ) }}" class="button alt">Más info</a></li>
+
+                    <li><a href="{{ action([App\Http\Controllers\CatalogController::class, 'getShow'], ['id' => $arrayProyectos[$i]->id] ) }}" class="button alt">Más info</a></li>
                 </ul>
             </footer>
         </section>
