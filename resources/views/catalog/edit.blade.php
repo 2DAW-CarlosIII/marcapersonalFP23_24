ddfa13bd6f534b2cedf8bf0c478bab9ef2e13794
MarianoConesa <1784440@alu.murciaeduca.es>
2023-12-02 17:07:02 +0100

    ej4.7

--- resources/views/catalog/edit.blade.php
+++ resources/views/catalog/edit.blade.php
@@ -10,31 +10,31 @@
          </div>
          <div class="card-body" style="padding:30px">
 
-            <form action="{{ action([App\Http\Controllers\CatalogController::class, 'putEdit'], ['id' => $id]) }}" method="POST">
+            <form action="{{ action([App\Http\Controllers\CatalogController::class, 'putEdit'], ['id' => $proyecto->id]) }}" method="POST">
 
 	            @csrf
                 @method('PUT')
 
 	            <div class="form-group">
 	               <label for="nombre">Nombre</label>
-	               <input type="text" name="nombre" id="nombre" value="{{$proyecto['nombre']}}" class="form-control">
+	               <input type="text" name="nombre" id="nombre" value="{{$proyecto->nombre}}" class="form-control">
 	            </div>
 
 	            <div class="form-group">
 	            	<label for="docente_id">Docente</label>
-	               <input type="number" name="docente_id" value="{{ $proyecto['docente_id'] }}" id="docente_id">
+	               <input type="number" name="docente_id" value="{{ $proyecto->docente_id}}" id="docente_id">
 	            </div>
 
 	            <div class="form-group">
 	            	<label for="dominio">Dominio</label><br />
                     https://github.com/2DAW-CarlosIII/
-	               <input type="text" name="dominio" id="dominio" value="{{ $proyecto['dominio'] }}" class="form-control">
+	               <input type="text" name="dominio" id="dominio" value="{{ $proyecto->dominio }}" class="form-control">
 	            </div>
 
 	            <div class="form-group">
 	               <label for="metadatos">Metadatos</label>
 	               <textarea name="metadatos" id="metadatos" class="form-control" value rows="3">
-                    {{print_r($proyecto['metadatos'],true)}}
+                    {{print_r($proyecto->metadatos,true)}}
                    </textarea>
 	            </div>
 
