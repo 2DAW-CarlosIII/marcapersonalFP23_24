40f123e886e71fe23f1f0b9e80639388c1a4e09d
Angel De Antonio <1868615@alu.murciaeduca.es>
2023-12-03 19:56:30 +0100

    Cambio de estructura for por foreach en el index

--- resources/views/docentes/index.blade.php
+++ resources/views/docentes/index.blade.php
@@ -4,26 +4,26 @@
 
 <div class="row">
 
-    @for ($i=0; $i<count($docente); $i++)
+    @foreach ($docente as $docent)
 
     <div class="col-4 col-6-medium col-12-small">
         <section class="box">
             <a href="#" class="image featured"><img src="{{ asset('/images/mp-logo.png') }}" alt="" /></a>
             <header>
-                <h3>{{ $docente[$i]->nombre }}</h3>
+                <h3>{{ $docent->nombre }}</h3>
             </header>
             <h4>
-                {{ $docente[$i]->departamento }}
+                {{ $docent->departamento }}
             </h4>
             <footer>
                 <ul class="actions">
-                    <li><a href="{{ action([App\Http\Controllers\DocenteController::class, 'getShow'], ['id' => $docente[$i]->id] ) }}" class="button alt">Más info</a></li>
+                    <li><a href="{{ action([App\Http\Controllers\DocenteController::class, 'getShow'], ['id' => $docent->id] ) }}" class="button alt">Más info</a></li>
                 </ul>
             </footer>
         </section>
     </div>
 
-    @endfor
+    @endforeach
 
 </div>
 @endsection
