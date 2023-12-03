d31da37023efb1b8eb54aa600eff908a895363a7
Antonio Amate <4252008@alu.murciaeduca.es>
2023-12-03 18:20:45 +0100

    4.7.3. Ejercicios de Bases de Datos

--- resources/views/reconocimientos/index.blade.php
+++ resources/views/reconocimientos/index.blade.php
@@ -1,32 +1,33 @@
 @extends('layouts.master')
 
 @section('content')
+    <div class="row">
 
-<div class="row">
+        @foreach ($reconocimientos as $reconocimiento)
+            <div class = "col-4 col-6-medium col-12-small">
+                <section class="box">
+                    <a href="#" class="image featured"
+                        title="Sakatsp, CC BY-SA 4.0 &lt;https://creativecommons.org/licenses/by-sa/4.0&gt;, via Wikimedia Commons"><img
+                            width="256" alt="Award icon"
+                            src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Award_icon.png"></a>
+                    <header>
+                        <h3>Estudiante {{ $reconocimiento->estudiante_id }}</h3>
+                    </header>
+                    <p>
 
-    @for ($i=0; $i<count($arrayReconocimientos); $i++)
+                        <a href="{{ $reconocimiento->documento }}">
+                            {{ $reconocimiento->documento }}
+                        </a>
+                    </p>
+                    <footer>
+                        <ul class="actions">
+                            <li><a href="{{ action([App\Http\Controllers\ReconocimientoController::class, 'getShow'], ['id' => $reconocimiento->id]) }}"
+                                    class="button alt">Más info</a></li>
+                        </ul>
+                    </footer>
+                </section>
+            </div>
+        @endforeach
 
-    <div class = "col-4 col-6-medium col-12-small">
-        <section class="box">
-            <a href="#" class="image featured" title="Sakatsp, CC BY-SA 4.0 &lt;https://creativecommons.org/licenses/by-sa/4.0&gt;, via Wikimedia Commons"><img width="256" alt="Award icon" src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Award_icon.png"></a>
-            <header>
-                <h3>Estudiante {{ $arrayReconocimientos[$i]['estudiante_id'] }}</h3>
-            </header>
-            <p>
-
-                <a href="{{ $arrayReconocimientos[$i]['documento'] }}">
-                    {{ $arrayReconocimientos[$i]['documento'] }}
-                </a>
-            </p>
-            <footer>
-                <ul class="actions">
-                    <li><a href="{{ action([App\Http\Controllers\ReconocimientoController::class, 'getShow'], ['id' => $i] ) }}" class="button alt">Más info</a></li>
-                </ul>
-            </footer>
-        </section>
     </div>
-
-    @endfor
-
-</div>
 @endsection
