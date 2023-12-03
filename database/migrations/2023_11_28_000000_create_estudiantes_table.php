385b80faae73b6866cc6671329d33f182cb4bf30
PedroDavid <4103404@alu.murciaeduca.es>
2023-12-04 00:14:34 +0100

    sol

--- database/migrations/2023_11_28_000000_create_estudiantes_table.php
+++ database/migrations/2023_11_28_000000_create_estudiantes_table.php
@@ -11,12 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-<<<<<<<<< Temporary merge branch 1
-        Schema::create('estudiantes', function($table)
-        {
-=========
-        Schema::create('estudiantes', function (Blueprint $table) {
->>>>>>>>> Temporary merge branch 2
+        Schema::create('estudiantes', function ($table) {
             $table->id();
             $table->string('nombre', 32);
             $table->string('apellidos', 32);
