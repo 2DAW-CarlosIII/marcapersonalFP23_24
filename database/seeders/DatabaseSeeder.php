b76fcdb01f2ce6fdc412fd0a3e4631171ad4eb00
Alejandro Martínez <11183658@alu.murciaeduca.es>
2023-12-03 16:02:26 +0100

    Solución ejerciciosDocentes4.7.1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(EstudiantesTableSeeder::class);
         // \App\Models\User::factory(10)->create();
+        $this->call(DocentesTableSeeder::class);
 
         \App\Models\User::factory()->create([
             'name' => 'Test User',
