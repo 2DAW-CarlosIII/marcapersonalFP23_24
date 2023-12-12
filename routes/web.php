3cd1fab89adfba2c3aaa70e114a47fe1014be594
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-12-12 08:54:50 +0100

    Terminando la explicación de autenticación

--- routes/web.php
+++ routes/web.php
@@ -25,24 +25,19 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('login', function () {
-    return view('auth.login');
-});
-
-Route::get('logout', function () {
-    return "Logout usuario";
-});
-
 Route::prefix('catalog')->group(function () {
     Route::get('/', [CatalogController::class, 'getIndex'])->name('proyectos');
 
     Route::get('/show/{id}', [CatalogController::class, 'getShow'])->where('id', '[0-9]+');
 
-    Route::get('/create', [CatalogController::class, 'getCreate']);
+    Route::get('/create', [CatalogController::class, 'getCreate'])
+    ->middleware('auth');
 
-    Route::get('/edit/{id}', [CatalogController::class, 'getEdit'])->where('id', '[0-9]+');
+    Route::get('/edit/{id}', [CatalogController::class, 'getEdit'])->where('id', '[0-9]+')
+    ->middleware('auth');
 
-    Route::put('/edit/{id}', [CatalogController::class, 'putEdit'])->where('id', '[0-9]+');
+    Route::put('/edit/{id}', [CatalogController::class, 'putEdit'])->where('id', '[0-9]+')
+    ->middleware('auth');
 });
 
 Route::prefix('reconocimientos')->group(function () {
