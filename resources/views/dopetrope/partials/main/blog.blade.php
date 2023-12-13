89b8e6d97294390b25806a1a012e2eec3f42d14c
Alejandro Martínez <11183658@alu.murciaeduca.es>
2023-12-13 09:54:16 +0100

    solucionLanding#209

--- resources/views/dopetrope/partials/main/blog.blade.php
+++ resources/views/dopetrope/partials/main/blog.blade.php
@@ -9,13 +9,13 @@
                         alt="" /></a>
                 <header>
                     <h3>Gestión de proyectos</h3>
-                    <p>Posted 45 minutes ago</p>
+
                 </header>
                 <p>En Marca Personal FP se permite la gestión de proyectos finales de fin de ciclo...</p>
                 <footer>
                     <ul class="actions">
                         <li><a href="{{ action([App\Http\Controllers\CatalogController::class, 'getIndex']) }}" class="button icon solid fa-file-alt">Continue Reading</a></li>
-                        <li><a href="#" class="button alt icon solid fa-comment">33 comments</a></li>
+
                     </ul>
                 </footer>
             </section>
