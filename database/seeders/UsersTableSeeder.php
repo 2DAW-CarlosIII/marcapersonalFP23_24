15c6f9ca0671bb70e48bf091e2ac1d1f48696e8c
Manuel Fernandez <1808965@alu.murciaeduca.es>
2023-12-13 08:03:04 +0100

    Ejercicio Breeze

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::truncate();
+
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => env('ADMIN_EMAIL', 'admin@email.com'),
+            'password' => env('ADMIN_PASSWORD', 'password'),
+        ]);
+
+    }
+}
