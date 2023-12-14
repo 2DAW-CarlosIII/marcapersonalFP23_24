5e142ce3ee1acea7c35b947f44f51b7e7bc5769f
Angel De Antonio <1868615@alu.murciaeduca.es>
2023-12-14 10:12:26 +0100

    MejoresProyectos

--- app/Models/Proyecto.php
+++ app/Models/Proyecto.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Proyecto extends Model
 {
     use HasFactory;
+
+    public static function mejoresProyectos($nProyectos) {
+        return self::orderBy('calificacion', 'desc')->take($nProyectos)->get();
+    }
 }
