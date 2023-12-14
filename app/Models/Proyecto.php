43723e12400707dffb9990153a56469e855824b4
Sergio Gutiérrez Valverde <1833914@alu.murciaeduca.es>
2023-12-14 09:05:12 +0100

    cambiado el footer con los mejores poryectos

--- app/Models/Proyecto.php
+++ app/Models/Proyecto.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Proyecto extends Model
 {
+    static function mejoresProyectos($num){
+        $consulta=DB::select("SELECT * FROM proyectos ORDER BY calificacion DESC LIMIT $num;");
+        return $consulta;
+    }
+
     use HasFactory;
 }
