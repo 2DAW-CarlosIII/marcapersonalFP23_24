c30b18db842accd0c224c71fd5190e756b89a28b
MFGalu <4136605@alu.murciaeduca.es>
2023-12-15 10:37:31 +0100

    Mostrar videocurrículo en la vista show y puestos en el array

--- database/seeders/CurriculosTableSeeder.php
+++ database/seeders/CurriculosTableSeeder.php
@@ -11,52 +11,52 @@ class CurriculosTableSeeder extends Seeder
     private static $arrayCurriculos = [
         [
             'user_id' => 1,
-            'video_curriculum' => 'https://www.youtube.com/watch?v=u54uern',
+            'video_curriculum' => 'Ds8lRXWfoPE',
             'texto_curriculum' => "Experiencia en desarrollo web con enfoque en tecnologías front-end.",
         ],
         [
             'user_id' => 2,
-            'video_curriculum' => 'https://www.youtube.com/watch?v=v87dfg2',
+            'video_curriculum' => 'fabDpTOfKns',
             'texto_curriculum' => "Habilidades avanzadas en HTML, CSS y JavaScript.",
         ],
         [
             'user_id' => 3,
-            'video_curriculum' => 'https://www.youtube.com/watch?v=frt32qe',
+            'video_curriculum' => '2in5XMTlSWg',
             'texto_curriculum' => "Amplia experiencia en el diseño y desarrollo de interfaces de usuario.",
         ],
         [
             'user_id' => 4,
-            'video_curriculum' => 'https://www.youtube.com/watch?v=wtrh2we',
+            'video_curriculum' => 'Sn-Za-l--e0',
             'texto_curriculum' => "Conocimientos sólidos en frameworks front-end como React y Vue.",
         ],
         [
             'user_id' => 5,
-            'video_curriculum' => 'https://www.youtube.com/watch?v=qwer123',
+            'video_curriculum' => 'T72HF3a2xL4',
             'texto_curriculum' => "Experiencia en integración de API y consumo de servicios web.",
         ],
         [
             'user_id' => 6,
-            'video_curriculum' => 'https://www.youtube.com/watch?v=ytgfd32',
+            'video_curriculum' => 'uTgscfzdbCc',
             'texto_curriculum' => "Habilidades de resolución de problemas y pensamiento lógico.",
         ],
         [
             'user_id' => 7,
-            'video_curriculum' => 'https://www.youtube.com/watch?v=zxvbn23',
+            'video_curriculum' => 'bIPgAncy3Q0',
             'texto_curriculum' => "Colaborador proactivo y eficiente en entornos de trabajo en equipo.",
         ],
         [
             'user_id' => 8,
-            'video_curriculum' => 'https://www.youtube.com/watch?v=asdf456',
+            'video_curriculum' => 'DCHp1F13R8k',
             'texto_curriculum' => "Capacidad para aprender rápidamente nuevas tecnologías y conceptos.",
         ],
         [
             'user_id' => 9,
-            'video_curriculum' => 'https://www.youtube.com/watch?v=qwerty78',
+            'video_curriculum' => 'nL7dnv_egaI',
             'texto_curriculum' => "Comprometido con la mejora continua y el desarrollo profesional.",
         ],
         [
             'user_id' => 10,
-            'video_curriculum' => 'https://www.youtube.com/watch?v=mnbvc90',
+            'video_curriculum' => 'AKLJxNRrvjc',
             'texto_curriculum' => "Comunicación efectiva y habilidades interpersonales.",
         ],
     ];
