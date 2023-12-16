82de42f415fea7077e668cb119290fe98913606c
DonHonorio <2918257@alu.murciaeduca.es>
2023-12-16 14:27:36 +0100

    Añadido miniatura del videocurriculum

--- resources/views/curriculos/index.blade.php
+++ resources/views/curriculos/index.blade.php
@@ -7,8 +7,8 @@
         <div class="col-4 col-6-medium col-12-small">
             <section class="box">
                 <a href="{{ action([App\Http\Controllers\CurriculoController::class, 'getShow'], ['id' => $curriculo->id] ) }}" class="image featured" title="SleaY, CC BY 4.0 &lt;https://creativecommons.org/licenses/by/4.0&gt;, via Wikimedia Commons">
-                    @if ($curriculo->avatar)
-                        <img width="256" src="{{ Storage::url($curriculo->avatar) }}" alt="Avatar" class="img-thumbnail">
+                    @if ($curriculo->video_curriculum)
+                        <img width="256" src="https://i.ytimg.com/vi/{{$curriculo->video_curriculum}}/hqdefault.jpg" alt="Avatar" class="img-thumbnail">
                     @else
                         <img width="256" alt="Curriculum-vitae-warning-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Curriculum-vitae-warning-icon.svg/256px-Curriculum-vitae-warning-icon.svg.png">
                     @endif
