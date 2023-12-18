54a3af76ae23b1987605bfbe084f22f91714b7eb
mdmmr97 <3730156@alu.murciaeduca.es>
2023-12-18 09:33:10 +0100

    Modificado enlace descarga curricullum

--- resources/views/curriculos/show.blade.php
+++ resources/views/curriculos/show.blade.php
@@ -11,30 +11,39 @@
     </div>
 
     <div class="col-sm-8  align-self-center">
+
         <h3><strong>Nombre:</strong> {{ $user->nombre }} {{ $user->apellidos }}</h3>
-    </br>
-    <h4><strong>Video Curriculum:</strong> </h4>
-    <iframe width="560" height="315"
-        src="https://www.youtube.com/embed/{{ $curriculo->video_curriculum }}"
-        title="YouTube video player"
-        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
-    </iframe>
+        </br>
 
-            @if ($curriculo->pdf_curriculum)
-                <a href="{{ Storage::url($curriculo->pdf_curriculum) }}" download="curriculo.pdf">Descargar currículo </a>
-            @else
-                <h4>Ningún currículo asociado</h4>
-            @endif </br>
-            <a class="btn btn-warning"
-                href="{{ action([App\Http\Controllers\CurriculoController::class, 'getEdit'], ['id' => $curriculo->id]) }}">
-                <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                Editar Curriculum
-            </a>
+        <h4 class="mb-2"><strong>Video Curriculum:</strong> </h4>
 
-            <a class="btn btn-outline-info"
-                href="{{ action([App\Http\Controllers\CurriculoController::class, 'getIndex']) }}">
-                Volver al listado
+        <iframe width="560" height="315"
+            src="https://www.youtube.com/embed/{{ $curriculo->video_curriculum }}"
+            title="YouTube video player"
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+        </iframe>
+        </br>
+
+        @if ($curriculo->pdf_curriculum)
+            <a class="btn btn-danger mb-3"
+                href="{{ Storage::url($curriculo->pdf_curriculum) }}" download="curriculo.pdf">
+                <i class="bi bi-download me-2"></i> Currículo
             </a>
+        @else
+            <h4 class="mb-3">Ningún currículo asociado</h4>
+        @endif
+        </br>
+
+        <a class="btn btn-warning"
+            href="{{ action([App\Http\Controllers\CurriculoController::class, 'getEdit'], ['id' => $curriculo->id]) }}">
+            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+            Editar Curriculum
+        </a>
+
+        <a class="btn btn-outline-info"
+            href="{{ action([App\Http\Controllers\CurriculoController::class, 'getIndex']) }}">
+            Volver al listado
+        </a>
         </div>
     </div>
 @endsection
