e2ca2a981403073ee4e7b8a0c7c658b190ea5ecc
Manuel Fernandez <1808965@alu.murciaeduca.es>
2023-12-18 09:34:48 +0100

    Error corregido de la vista show de currículum

--- resources/views/curriculos/show.blade.php
+++ resources/views/curriculos/show.blade.php
@@ -1,27 +1,31 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="row m-4">
-    <div class="col-sm-4">
-        @if ($user->avatar)
-            <img width="300" style="height:300px" src="{{ Storage::url($user->avatar) }}" alt="Avatar" class="img-thumbnail">
-        @else
-            <img width="300" style="height:300px" alt="Curriculum-vitae-warning-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Curriculum-vitae-warning-icon.svg/256px-Curriculum-vitae-warning-icon.svg.png">
-        @endif
-    </div>
+    <div class="row m-4">
+        <div class="col-sm-4">
+            @if ($user->avatar)
+                <img width="300" style="height:300px" src="{{ Storage::url($user->avatar) }}" alt="Avatar"
+                    class="img-thumbnail">
+            @else
+                <img width="300" style="height:300px" alt="Curriculum-vitae-warning-icon"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Curriculum-vitae-warning-icon.svg/256px-Curriculum-vitae-warning-icon.svg.png">
+            @endif
+        </div>
 
-    <div class="col-sm-8  align-self-center">
-        <h3><strong>Nombre:</strong> {{ $user->nombre }} {{ $user->apellidos }}</h3>
-    </br>
-    <h4><strong>Video Curriculum:</strong> </h4>
-    <iframe width="560" height="315"
-        src="https://www.youtube.com/embed/{{ $curriculo->video_curriculum }}"
-        title="YouTube video player"
-        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
-    </iframe>
+        <div class="col-sm-8  align-self-center">
+            <h3><strong>Nombre:</strong> {{ $user->nombre }} {{ $user->apellidos }}</h3>
+            </br>
+            <h4><strong>Video Curriculum:</strong> </h4>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/{{ $curriculo->video_curriculum }}"
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen>
+            </iframe>
 
             @if ($curriculo->pdf_curriculum)
+                <br>
                 <a href="{{ Storage::url($curriculo->pdf_curriculum) }}" download="curriculo.pdf">Descargar currículo </a>
+                <br>
             @else
                 <h4>Ningún currículo asociado</h4>
             @endif </br>
