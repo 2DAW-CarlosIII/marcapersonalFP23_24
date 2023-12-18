a1760026dec38a5f73d3fdea373a20a22b373339
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-12-18 09:08:19 +0100

    Moficacion del id utilizado para actualizar el curriculo y el campo del formulario

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Curriculo;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,8 +17,11 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): View
     {
+        $user = $request->user();
+        $curriculo = Curriculo::where('user_id', $user->id)->first();
         return view('profile.edit', [
-            'user' => $request->user(),
+            'user' => $user,
+            'curriculo' => $curriculo
         ]);
     }
 
