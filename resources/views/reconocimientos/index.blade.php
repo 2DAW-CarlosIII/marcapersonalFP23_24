ac32b73d5fb3b9a316fb18a7ac8b9bb505e40b49
Angel De Antonio <1868615@alu.murciaeduca.es>
2023-12-19 09:55:16 +0100

    Cambios

--- resources/views/reconocimientos/index.blade.php
+++ resources/views/reconocimientos/index.blade.php
@@ -10,8 +10,10 @@
         <section class="box">
             <a href="#" class="image featured" title="Sakatsp, CC BY-SA 4.0 &lt;https://creativecommons.org/licenses/by-sa/4.0&gt;, via Wikimedia Commons"><img width="256" alt="Award icon" src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Award_icon.png"></a>
             <header>
-                <h3>Estudiante: {{ $reconocimiento->estudiante_nombre}} {{ $reconocimiento->estudiante_apellidos}}</h3>
-                <h3>Actividad: {{ $reconocimiento->actividad_nombre}} </h3>
+                <h3>Estudiante: </h3>
+                <p style="font-size: 1.3em">{{ $reconocimiento->estudiante_nombre}} {{ $reconocimiento->estudiante_apellidos}}</p>
+                <h3>Actividad: </h3>
+                <p style="font-size: 1.3em">{{ $reconocimiento->actividad_nombre}}</p>
             </header>
             <p>
 
