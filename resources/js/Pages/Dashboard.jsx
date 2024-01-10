57105487cdf3d13533a2789b7ee88814be2bd61c
mdmmr97 <3730156@alu.murciaeduca.es>
2024-01-10 09:53:39 +0100

    Reactadmin instalado con Inertia

---
+++ resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { App } from "../react-admin/App";
+
+const Dashboard = () => {
+    return(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+}
+
+export default Dashboard;
