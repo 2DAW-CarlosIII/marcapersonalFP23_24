db7f0cf2211b0c3d12f0aa69b13440072a6b1f23
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-01-10 17:44:01 +0100

    Personalizando el componente Resource

--- resources/js/react-admin/App.tsx
+++ resources/js/react-admin/App.tsx
@@ -2,16 +2,22 @@
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
 import { dataProvider } from './dataProvider';
 import { authProvider } from './authProvider';
+import { ProyectoList, ProyectoEdit, ProyectoShow, ProyectoCreate } from './pages/proyectos';
+import ProyectoIcon from '@mui/icons-material/AccountTree';
 
 export const App = () => (
     <Admin
         dataProvider={dataProvider}
 		authProvider={authProvider}
 	>
-        <Resource name="proyectos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="proyectos"
+            icon={ProyectoIcon}
+            list={ProyectoList}
+            edit={ProyectoEdit}
+            show={ProyectoShow}
+            create={ProyectoCreate}
+        />
         <Resource name="users" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="actividades" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
     </Admin>
 );
-
-    
