966bb5f86aed676e650c2762b7e434ad11e457f3
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-01-11 19:59:55 +0100

    Añadiendo todas las tablas al dashboard

--- resources/js/react-admin/App.tsx
+++ resources/js/react-admin/App.tsx
@@ -8,8 +8,13 @@ export const App = () => (
         dataProvider={dataProvider}
 		authProvider={authProvider}
 	>
+        <Resource name="users" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="curriculos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="proyectos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="ciclos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="familias_profesionales" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="actividades" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="reconocimientos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
     </Admin>
 );
 
-    
