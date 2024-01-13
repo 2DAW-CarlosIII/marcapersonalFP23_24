009912cddf46aff0adeddbbb3e2260d6cddb8fe5
Jonatan <1732367@alu.murciaeduca.es>
2024-01-13 16:23:24 +0100

    Modificado la vista de la tabla ciclo del dashboard

--- resources/js/react-admin/App.tsx
+++ resources/js/react-admin/App.tsx
@@ -2,6 +2,8 @@
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
 import { dataProvider } from './dataProvider';
 import { authProvider } from './authProvider';
+import CicloIcon from '../Pages/iconos/IconoCiclo';
+import { CicloCreate, CicloEdit, CicloList, CicloShow } from '../Pages/Ciclos';
 
 export const App = () => (
     <Admin
@@ -11,7 +13,7 @@ export const App = () => (
         <Resource name="users" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="curriculos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="proyectos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-        <Resource name="ciclos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="ciclos" list={CicloList} edit={CicloEdit} show={CicloShow} create={CicloCreate} icon={CicloIcon} />
         <Resource name="familias_profesionales" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="actividades" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="reconocimientos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
