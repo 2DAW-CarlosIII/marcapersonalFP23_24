bc938687e1560cd221db926c69891575ddb28ac5
Alejandro <5568981@alu.murciaeduca.es>
2024-01-14 19:45:31 +0100

    DashBoard/Proyectos/Ciclos

--- resources/js/react-admin/App.tsx
+++ resources/js/react-admin/App.tsx
@@ -2,7 +2,11 @@
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
 import { dataProvider } from './dataProvider';
 import { authProvider } from './authProvider';
+import CicloIcon from '../Pages/iconos/CicloIcons';
+import { CicloCreate, CicloEdit, CicloList, CicloShow } from '../Pages/Ciclos'
 
+import { ProyectoList, ProyectoEdit, ProyectoShow, ProyectoCreate } from '../Pages/Proyectos';
+import ProyectoIcon from '@mui/icons-material/AccountTree';
 export const App = () => (
     <Admin
         dataProvider={dataProvider}
@@ -10,11 +14,15 @@ export const App = () => (
 	>
         <Resource name="users" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="curriculos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-        <Resource name="proyectos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-        <Resource name="ciclos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="proyectos"
+          icon={ProyectoIcon}
+          list={ProyectoList}
+          edit={ProyectoEdit}
+          show={ProyectoShow}
+          create={ProyectoCreate}/>
+        <Resource name="ciclos" list={CicloList} edit={CicloEdit} show={CicloShow} create={CicloCreate} icon={CicloIcon} />
         <Resource name="familias_profesionales" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="actividades" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="reconocimientos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
     </Admin>
 );
-
