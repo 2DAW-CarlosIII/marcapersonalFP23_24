cf6023116735bf9d8da40953d9b61f9e0300ae93
Miguel <5514787@alu.murciaeduca.es>
2024-01-15 00:18:51 +0100

    Añadida la tabla reconocimientos al dashboard react-admin

---
+++ resources/js/Pages/reconocimientos.jsx
@@ -0,0 +1,137 @@
+import { useRecordContext} from 'react-admin';
+import { useMediaQuery } from '@mui/material';
+
+import {
+    List,
+    SimpleList,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    TextInput,
+    NumberField,
+    NumberInput,
+    FunctionField,
+    SelectInput,
+    ShowButton,
+    Show,
+    SimpleShowLayout
+  } from 'react-admin';
+
+const TutorInput = () => (
+    <ReferenceInput label="Tutor" source="docente_id" reference="users">
+        <SelectInput
+        label="Tutor"
+        source="docente_id"
+        optionText={record => record && `${record.nombre} ${record.apellidos}`} />
+    </ReferenceInput>
+)
+const EstudianteInput = () => (
+    <ReferenceInput label="Estudiante" source="estudiante_id" reference="users">
+        <SelectInput
+        label="Estudiante"
+        source="estudiante_id"
+        optionText={record => record && `${record.nombre} ${record.apellidos}`} />
+    </ReferenceInput>
+)
+const ActividadInput = () => (
+    <ReferenceInput label="Actividad" source="actividad_id" reference="proyectos">
+        <SelectInput
+        label="Actividad"
+        source="actividad_id"
+        optionText={record => record && `${record.nombre}`} />
+    </ReferenceInput>
+)
+const reconocimientosFilters = [
+    <TextInput source="q" label="Search" alwaysOn />,
+    EstudianteInput(),
+];
+
+export const ReconocimientoList = () => {
+  const isSmall = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+  return (
+    <List filters={reconocimientosFilters} >
+      {isSmall ? (
+        <SimpleList
+          primaryText="%{estudiante_id}"
+          secondaryText="%{actividad_id}"
+          tertiaryText="%{docente_validador}"
+          linkType={(record) => (record.canEdit ? 'edit' : 'show')}
+        >
+          <EditButton />
+        </SimpleList>
+      ) : (
+        <Datagrid bulkActionButtons={false} >
+          <TextField source="id" />
+          <ReferenceField label="Estudiante" source="estudiante_id" reference="users">
+            <FunctionField render={record => record && `${record.nombre} ${record.apellidos}`} />
+          </ReferenceField>
+          <ReferenceField label="Actividad" source="actividad_id" reference="actividades">
+            <FunctionField render={record => record && `${record.nombre}`} />
+          </ReferenceField>
+          <TextField source="documento" />
+          <ReferenceField label="Tutor" source="docente_validador" reference="users">
+            <FunctionField render={record => record && `${record.nombre} ${record.apellidos}`} />
+          </ReferenceField>
+          <ShowButton />
+          <EditButton />
+        </Datagrid>
+      )}
+    </List>
+  );
+}
+
+export const ReconocimientoTitle = () => {
+    const record = useRecordContext();
+    return <span>Recomocimiento {record ? `"${record.nombre}"` : ''}</span>;
+  };
+
+  export const ReconocimientoEdit = () => (
+    <Edit title={<ReconocimientoTitle />}>
+    <SimpleForm>
+        <TextInput source="id" disabled />
+        <EstudianteInput />
+        <ActividadInput />
+        <TextInput source="documento" />
+        <DocenteInput />
+        <TextInput source="fecha" />
+    </SimpleForm>
+    </Edit>
+);
+
+export const ReconocimientoShow = () => (
+    <Show>
+        <SimpleShowLayout>
+            <TextField source="id" />
+            <ReferenceField label="Estudiante" source="estudiante_id" reference="users">
+                <FunctionField render={record => record && `${record.nombre} ${record.apellidos}`} />
+            </ReferenceField>
+            <ReferenceField label="Actividad" source="actividad_id" reference="users">
+                <FunctionField render={record => record && `${record.nombre}`} />
+            </ReferenceField>
+            <TextField source="documento" />
+            <ReferenceField label="Docente" source="docente_validador" reference="users">
+                <FunctionField render={record => record && `${record.nombre} ${record.apellidos}`} />
+            </ReferenceField>
+            <TextField source="fecha" />
+        </SimpleShowLayout>
+    </Show>
+);
+
+
+export const ReconocimientoCreate = () => (
+    <Create>
+        <SimpleForm>
+            <TextInput source="id" disabled />
+            <EstudianteInput />
+            <ActividadInput />
+            <TextInput source="documento" />
+            <DocenteInput />
+            <TextInput source="fecha" />
+        </SimpleForm>
+    </Create>
+);
--- resources/js/react-admin/App.tsx
+++ resources/js/react-admin/App.tsx
@@ -2,12 +2,26 @@
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
 import { dataProvider } from './dataProvider';
 import { authProvider } from './authProvider';
+import { ReconocimientoList, ReconocimientoEdit, ReconocimientoShow, ReconocimientoCreate } from '../Pages/reconocimientos';
+import ReconocimientoIcon from '@mui/icons-material/School';
 
 export const App = () => (
     <Admin
         dataProvider={dataProvider}
 		authProvider={authProvider}
 	>
+        <Resource name="users" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="curriculos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="ciclos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="familias_profesionales" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="actividades" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="proyectos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="reconocimientos"
+            icon={ReconocimientoIcon}
+            list={ReconocimientoList}
+            edit={ReconocimientoEdit}
+            show={ReconocimientoShow}
+            create={ReconocimientoCreate}
+        />
     </Admin>
 );
