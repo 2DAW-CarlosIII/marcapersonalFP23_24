3c307bf0241b2d8f74bad0d6c219752ba0408c91
DonHonorio <2918257@alu.murciaeduca.es>
2024-01-21 16:42:15 +0100

    Filtrar por campos relacionados

--- app/Http/Controllers/API/CicloController.php
+++ app/Http/Controllers/API/CicloController.php
@@ -17,7 +17,8 @@ class CicloController extends Controller
     public function index(Request $request)
     {
         $campos = ['nombre'];
-        $query = FilterHelper::applyFilter($request, $campos);
+        $otrosFiltros = ['familia_id'];
+        $query = FilterHelper::applyFilter($request, $campos, $otrosFiltros);
 
         return CicloResource::collection(
             $query->orderBy($request->_sort ?? 'id', $request->_order ?? 'asc')
