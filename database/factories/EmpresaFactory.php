4e6d0bc4ac78b413505320ee5839870437b005ae
Jose7420 <7962446@alu.murciaeduca.es>
2024-01-22 09:49:43 +0100

    Issue Empresas 486

---
+++ database/factories/EmpresaFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Empresa>
+ */
+class EmpresaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nif' => $this->faker->unique()->regexify('[0-9]{8}[A-Z]{1}'),
+            'email' => $this->faker->safeEmail(),
+            'password' => $this->faker->password(),
+
+        ];
+    }
+}
---
+++ database/seeders/EmpresaTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Empresa;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EmpresaTablaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Empresa::truncate();
+        Empresa::factory(10)->create();
+
+
+    }
+}
