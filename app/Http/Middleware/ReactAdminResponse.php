9d3065041f8f55691003fc7625384458baddaf87
JonatanMolina91 <1732367@alu.murciaeduca.es>
2024-01-23 08:35:36 +0100

    Corregido la comprobacion si existe countFiltro

--- app/Http/Middleware/ReactAdminResponse.php
+++ app/Http/Middleware/ReactAdminResponse.php
@@ -25,7 +25,7 @@ class ReactAdminResponse
         }
         $response = $next($request);
         if($request->routeIs('*.index')) {
-            abort_unless(property_exists($request->route()->controller, 'modelclass'), 500, "It must exists a modelclass property in the controller.");
+            abort_unless(property_exists($request->route()->controller, 'countFiltro'), 500, "It must exists a modelclass property in the controller.");
             $modelClassName = $request->route()->controller->countFiltro;
             $response->header('X-Total-Count',$modelClassName);
         }
