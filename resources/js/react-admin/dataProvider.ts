cd7cb02fc207628ae21c4d12e4e1508dbd4e3e8d
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-01-24 17:37:04 +0100

    Enviando los ids como arrays en el query

--- resources/js/react-admin/dataProvider.ts
+++ resources/js/react-admin/dataProvider.ts
@@ -1,5 +1,24 @@
+import exp from 'constants';
 import jsonServerProvider from 'ra-data-json-server';
+import { stringify } from 'query-string';
+import { fetchUtils } from 'ra-core';
 
-export const dataProvider = jsonServerProvider(
-    import.meta.env.VITE_JSON_SERVER_URL
+const apiUrl = import.meta.env.VITE_JSON_SERVER_URL;
+
+const dataProvider = jsonServerProvider(
+    apiUrl
 );
+
+const httpClient = (url, options = {}) => {
+    return fetchUtils.fetchJson(url, options);
+};
+
+dataProvider.getMany = (resource, params) => {
+    const query = {
+        id: params.ids,
+    };
+    const url = `${apiUrl}/${resource}?${stringify(query, {arrayFormat: 'bracket'})}`;
+    return httpClient(url).then(({ json }) => ({ data: json }));
+}
+
+export { dataProvider };
