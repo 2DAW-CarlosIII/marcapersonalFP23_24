380a8c941556e371ab0572bb0fc4319fab315252
DonHonorio <2918257@alu.murciaeduca.es>
2024-01-25 16:10:13 +0100

    Issue seed users_competencias

--- database/seeders/UsersCompetenciasTableSeeder.php
+++ database/seeders/UsersCompetenciasTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Competencia;
+use App\Models\User;
 use App\Models\UserCompetencia;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,6 +16,21 @@ class UsersCompetenciasTableSeeder extends Seeder
     public function run(): void
     {
         UserCompetencia::truncate();
-        UserCompetencia::factory(5)->create();
+
+        //Vamos a asignarle a todos los usuarios 0,1 o 2 competencias aleatorias
+        $competencias = Competencia::all();
+        $users = User::all();
+
+        foreach ($users as $user) {
+            //numero de registros a insertar (competencias)
+            $numRegistros = rand(0, 2);
+
+            for ($i=0; $i < $numRegistros; $i++) {
+                $user->competencias()->attach($competencias->random()->id);
+            }
+        }
+
+
+
     }
 }
