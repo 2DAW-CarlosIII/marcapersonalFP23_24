37a8fd8b0c31cda98591472d1b7f97247d86dbb1
pytocino <4103404@alu.murciaeduca.es>
2024-01-25 08:49:45 +0100

    Añadida la relacion entre users y proyectos

--- app/Models/Proyecto.php
+++ app/Models/Proyecto.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Proyecto extends Model
 {
@@ -22,8 +24,14 @@ class Proyecto extends Model
         $nProyectos = self::orderByDesc('calificacion')->take(5)->get();
         return $nProyectos;
     }
-    public static function contarProyectos(){
+    public static function contarProyectos()
+    {
         $proyectos = self::all()->count();
         return $proyectos;
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -49,7 +50,7 @@ class User extends Authenticatable
 
     /**
      * Get the curriculo associated with the user.
-    */
+     */
     public function curriculo(): HasOne
     {
         return $this->hasOne(Curriculo::class);
@@ -57,10 +58,15 @@ class User extends Authenticatable
 
     /**
      * The idiomas that belong to the user.
-    */
+     */
     public function idiomas(): BelongsToMany
     {
         return $this->belongsToMany(Idioma::class, 'users_idiomas', 'user_id', 'idioma_id')
-        ->withPivot(['nivel', 'certificado']);
+            ->withPivot(['nivel', 'certificado']);
+    }
+
+    public function proyectos(): BelongsToMany
+    {
+        return $this->belongsToMany(Proyecto::class, 'participantes_proyectos', 'estudiante_id', 'proyecto_id');
     }
 }
