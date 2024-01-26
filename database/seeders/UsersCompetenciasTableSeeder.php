cfe02315b978594ca9d95dd5918e0f5d656b8a2b
Jesus <4186790@alu.murciaeduca.es>
2024-01-26 17:51:16 +0100

    Terminado

--- database/seeders/UsersCompetenciasTableSeeder.php
+++ database/seeders/UsersCompetenciasTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Competencia;
+use App\Models\User;
 use App\Models\UserCompetencia;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,6 +16,21 @@ class UsersCompetenciasTableSeeder extends Seeder
     public function run(): void
     {
         UserCompetencia::truncate();
-        UserCompetencia::factory(5)->create();
+
+        $listaUsuarios = User::all();
+        $listaCompetencias = Competencia::all();
+
+        // A cada usuario se le tiene que asignar 0, 1 o 2 competencias
+
+        foreach ($listaUsuarios as $usuario) {
+            $numeroCompetenciasUsuario = random_int(0, 2);
+
+            for ($i = 0; $i < $numeroCompetenciasUsuario; $i++){
+                $competencia = $listaCompetencias->random();
+                $usuario->competencias()->attach($competencia->id);
+            }
+        }
+
+        // TODO No sé si habría que asociar también un docente validador, pero ahora mismo lo único que se podría hacer es escoger un user aleatorio
     }
 }
