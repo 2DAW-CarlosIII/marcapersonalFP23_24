fbaf37e3569a19f0e4ea3aaa429bb181b3ad3782
Zaidanxp <3720290@alu.murciaeduca.es>
2024-01-27 20:09:12 +0100

    Corrección en caso de que un usuario tengo dos veces el mismo proyecto asociado

--- database/seeders/ParticipantesProyectosTableSeeder.php
+++ database/seeders/ParticipantesProyectosTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 use App\Models\ParticipanteProyecto;
+use App\Models\Proyecto;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,15 +15,25 @@ class ParticipantesProyectosTableSeeder extends Seeder
     public function run(): void
     {
         ParticipanteProyecto::truncate();
+
         $users = User::all();
         foreach($users as $user){
 
-            $userProyects = random_int(0,2);
+            $proyectosSeleccionados = Proyecto::all()->random(random_int(0,2));
 
-            for($iterator = 0; $iterator < $userProyects; $iterator++){
+            $sonProyectosDiferentes = false;
 
-                $user->proyectos()->attach(User::inRandomOrder()->first()->id);
+            if(count($proyectosSeleccionados) == 2){
+                do{
+
+                    if($proyectosSeleccionados->get(0)->id !== $proyectosSeleccionados->get(1)->id){
+                        $sonProyectosDiferentes = true;
+                    }
+
+                }while(!$sonProyectosDiferentes);
             }
+
+            $user->proyectos()->attach($proyectosSeleccionados);
 
         }
 
