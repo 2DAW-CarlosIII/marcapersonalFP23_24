fa403cc8f8d3a037d9354cfebea57f6e51802246
mdmmr97 <3730156@alu.murciaeduca.es>
2024-01-27 20:42:53 +0100

    Modificada seed users_competencias para relaciones

--- database/seeders/UsersCompetenciasTableSeeder.php
+++ database/seeders/UsersCompetenciasTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\UserCompetencia;
+use App\Models\User;
+use App\Models\Competencia;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,19 @@ class UsersCompetenciasTableSeeder extends Seeder
     public function run(): void
     {
         UserCompetencia::truncate();
-        UserCompetencia::factory(5)->create();
+
+        $users = User::all();
+        $competencias = Competencia::all();
+
+        foreach ($users as $user) {
+            $ncompetencias = random_int(0,2);
+            for ($i=0; $i < $ncompetencias; $i++) {
+                $competencia = $competencias->random();
+                while ($user->competencias->contains($competencia)) {
+                    $competencia = $competencias->random();
+                }
+                $user->competencias()->attach($competencia->id);
+            }
+        }
     }
 }
