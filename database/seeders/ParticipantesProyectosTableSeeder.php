a7de9c07fbf81202c42c22924b8482836743893c
Jose7420 <7962446@alu.murciaeduca.es>
2024-01-28 09:50:40 +0100

    Issues_seed participantes_proyectos_562

--- database/seeders/ParticipantesProyectosTableSeeder.php
+++ database/seeders/ParticipantesProyectosTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 use App\Models\ParticipanteProyecto;
+use App\Models\Proyecto;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,7 +14,22 @@ class ParticipantesProyectosTableSeeder extends Seeder
      */
     public function run(): void
     {
+        // Truncate en la tabla para eliminar datos previos
         ParticipanteProyecto::truncate();
-        ParticipanteProyecto::factory(5)->create();
+
+        // coje todos los estudiantes y proyectos
+        $estudiantes = User::all();
+        $proyectos = Proyecto::all();
+
+        // Para cada estudiante, asignar 0, 1 o 2 proyectos aleatorios
+        foreach ($estudiantes as $estudiante) {
+            $proyectosAleatorios = $proyectos->random(rand(0, 2));
+            foreach ($proyectosAleatorios as $proyecto) {
+                ParticipanteProyecto::firstOrCreate([
+                    'estudiante_id' => $estudiante->id,
+                    'proyecto_id' => $proyecto->id,
+                ]);
+            }
+        }
     }
 }
