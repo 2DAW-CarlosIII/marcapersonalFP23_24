d887a5cdf4c5f13f396bfe3b7a0ad184b3ceb68b
Jonatan <1732367@alu.murciaeduca.es>
2024-01-28 16:41:14 +0100

    seed users_competencias #559

--- database/seeders/UsersCompetenciasTableSeeder.php
+++ database/seeders/UsersCompetenciasTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Competencia;
+use App\Models\User;
 use App\Models\UserCompetencia;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,6 +16,14 @@ class UsersCompetenciasTableSeeder extends Seeder
     public function run(): void
     {
         UserCompetencia::truncate();
-        UserCompetencia::factory(5)->create();
+        $usuarios = User::all();
+        $competencias = Competencia::all();
+
+        foreach ($usuarios as $usuario) {
+            for ($i = 0; $i < random_int(0, 2); $i++){
+                $competencia = $competencias->random();
+                $usuario->competencias()->attach($competencia->id);
+            }
+        }
     }
 }
