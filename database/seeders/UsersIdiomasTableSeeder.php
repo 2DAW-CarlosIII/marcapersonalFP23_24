7b05ef9613a684c3856e38f3d0d6535112512432
Miguel <5514787@alu.murciaeduca.es>
2024-01-29 10:12:57 +0100

    Issue Idiomas Users seeder

--- database/seeders/UsersIdiomasTableSeeder.php
+++ database/seeders/UsersIdiomasTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Idioma;
+use App\Models\User;
 
 class UsersIdiomasTableSeeder extends Seeder
 {
@@ -16,15 +18,22 @@ class UsersIdiomasTableSeeder extends Seeder
         DB::table('users_idiomas')->truncate();
         $nivel = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'];
 
-        foreach(DB::table('users')->get() as $user) {
-            $idiomas = DB::table('idiomas')->get();
-            $idioma = $idiomas[rand(0, count($idiomas)-1)];
-            DB::table('users_idiomas')->insert([
-                'user_id' => $user->id,
-                'idioma_id' => $idioma->id,
-                'nivel' => $nivel[rand(0, count($nivel)-1)],
-                'certificado' => rand(0,1),
-            ]);
+        $users = User::all();
+        $idiomas = Idioma::all();
+
+        foreach ($users as $user) {
+            $num_idiomas = rand(0, 2);
+
+            for ($i = 0; $i < $num_idiomas; $i++) {
+                $idioma = $idiomas->random();
+
+                if (!$user->idiomas->contains($idioma)) {
+                    $user->idiomas()->attach($idioma->id, [
+                        'nivel' => rand(1, 5),
+                        'certificado' => rand(0, 1),
+                    ]);
+                }
+            }
         }
     }
 }
