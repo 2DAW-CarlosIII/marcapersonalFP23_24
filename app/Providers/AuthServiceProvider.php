89934a89d7380e60eb136cd131eee0081bb35071
Jose7420 <7962446@alu.murciaeduca.es>
2024-02-02 13:03:12 +0100

    Issues Autorizacion empresa_612

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use App\Models\Curriculo;
+use App\Models\Empresa;
 use App\Policies\CurriculoPolicy;
+use App\Policies\EmpresaPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -16,6 +18,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Curriculo::class => CurriculoPolicy::class,
+        Empresa::class => EmpresaPolicy::class,
     ];
 
     /**
