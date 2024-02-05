589292ffd90bb000a5bc940e5d7a9cb8fbd009a3
Andrea Escudero <7460086@alu.murciaeduca.es>
2024-02-05 09:58:46 +0100

    validacion reconocimientos

--- app/Http/Controllers/API/ReconocimientoController.php
+++ app/Http/Controllers/API/ReconocimientoController.php
@@ -1,87 +1,101 @@
 <?php
 
+
 namespace App\Http\Controllers\API;
+
 
 use App\Helpers\FilterHelper;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ReconocimientoResource;
 use App\Models\Reconocimiento;
+use App\Models\User;
 use Illuminate\Http\Request;
+
 
 class ReconocimientoController extends Controller
 {
-    public $modelclass = Reconocimiento::class;
+   public $modelclass = Reconocimiento::class;
 
-    /**
-     * Create the controller instance.
-     *
-     * @return void
-     */
 
-    public function __construct()
-    {
-        $this->middleware('auth:sanctum')->except(['index', 'show']);
-        $this->authorizeResource(Reconocimiento::class, 'reconocimiento');
-    }
+   /**
+    * Create the controller instance.
+    *
+    * @return void
+    */
 
-    public function index(Request $request)
-    {
-        $campos = [];
-        $otrosFiltros = ['estudiante_id', 'actividad_id', 'docente_validador'];
-        $query = FilterHelper::applyFilter($request, $campos, $otrosFiltros);
-        $request->attributes->set('total_count', $query->count());
-        $queryOrdered = FilterHelper::applyOrder($query, $request);
-        return ReconocimientoResource::collection(
-            $query->orderBy($request->_sort ?? 'id', $request->_order ?? 'asc')
-                ->paginate($request->perPage)
-        );
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $reconocimiento = json_decode($request->getContent(), true);
-        $reconocimiento = Reconocimiento::create($reconocimiento);
+   public function __construct()
+   {
+       $this->middleware('auth:sanctum')->except(['index', 'show']);
+       $this->authorizeResource(Reconocimiento::class, 'reconocimiento');
+   }
 
-        return new ReconocimientoResource($reconocimiento);
-    }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Reconocimiento $reconocimiento)
-    {
-        return new ReconocimientoResource($reconocimiento);
-    }
+   public function index(Request $request)
+   {
+       $campos = [];
+       $otrosFiltros = ['estudiante_id', 'actividad_id', 'docente_validador'];
+       $query = FilterHelper::applyFilter($request, $campos, $otrosFiltros);
+       $request->attributes->set('total_count', $query->count());
+       $queryOrdered = FilterHelper::applyOrder($query, $request);
+       return ReconocimientoResource::collection(
+           $query->orderBy($request->_sort ?? 'id', $request->_order ?? 'asc')
+               ->paginate($request->perPage)
+       );
+   }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Reconocimiento $reconocimiento)
-    {
-        $reconocimientoData = json_decode($request->getContent(), true);
-        $reconocimiento->update($reconocimientoData);
 
-        return new ReconocimientoResource($reconocimiento);
-    }
+   /**
+    * Store a newly created resource in storage.
+    */
+   public function store(Request $request)
+   {
+       if ($request->user()->esDocente()) {
+           $reconocimiento = json_decode($request->getContent(), true);
+           $reconocimiento = Reconocimiento::create($reconocimiento);
+           return new ReconocimientoResource($reconocimiento);
+       }elseif($request->user()->esAdmin()){
+           $reconocimiento = json_decode($request->getContent(), true);
+           $reconocimiento = Reconocimiento::create($reconocimiento);
+           return new ReconocimientoResource($reconocimiento);
+       }else{
+           $reconocimiento = json_decode($request->getContent(), true);
+           $reconocimiento['docente_validador'] = null;
+           $reconocimiento = Reconocimiento::create($reconocimiento);
+           return new ReconocimientoResource($reconocimiento);
+       }
 
-    public function validar(Request $request, Reconocimiento $reconocimiento){
 
-        $this->authorize('validar', $reconocimiento);
-        $reconocimientoData = json_decode($request->getContent('docente_id'), true);
-        $reconocimientoData['docente_id']= auth()->id();
-        $reconocimiento->update($reconocimientoData);
+   }
 
-        return new ReconocimientoResource($reconocimiento);
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Reconocimiento $reconocimiento)
-    {
-        $reconocimiento->delete();
-    }
+   /**
+    * Display the specified resource.
+    */
+   public function show(Reconocimiento $reconocimiento)
+   {
+       return new ReconocimientoResource($reconocimiento);
+   }
+
+
+   /**
+    * Update the specified resource in storage.
+    */
+   public function update(Request $request, Reconocimiento $reconocimiento)
+   {
+       $reconocimientoData = json_decode($request->getContent(), true);
+       $reconocimiento->update($reconocimientoData);
+
+
+       return new ReconocimientoResource($reconocimiento);
+   }
+
+
+   /**
+    * Remove the specified resource from storage.
+    */
+   public function destroy(Reconocimiento $reconocimiento)
+   {
+       $reconocimiento->delete();
+   }
 }
--- app/Policies/ReconocimientoPolicy.php
+++ app/Policies/ReconocimientoPolicy.php
@@ -1,77 +1,89 @@
 <?php
 
+
 namespace App\Policies;
+
 
 use App\Models\Reconocimiento;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
+
 class ReconocimientoPolicy
 {
 
-    public function before(User $user, $ability)
-    {
-        if($user->esAdmin()) return true;
-    }
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(?User $user): bool
-    {
-        return true;
-    }
 
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(?User $user, Reconocimiento $reconocimiento): bool
-    {
-        return true;
-    }
+   public function before(User $user, $ability)
+   {
+       if($user->esAdmin()) return true;
+   }
+   /**
+    * Determine whether the user can view any models.
+    */
+   public function viewAny(?User $user): bool
+   {
+       return true;
+   }
 
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        return $user->esEstudiante();
-    }
 
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Reconocimiento $reconocimiento): bool
-    {
-        return $user->esPropietario($reconocimiento,$reconocimiento->estudiante_id);
-    }
+   /**
+    * Determine whether the user can view the model.
+    */
+   public function view(?User $user, Reconocimiento $reconocimiento): bool
+   {
+       return true;
+   }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Reconocimiento $reconocimiento): bool
-    {
-        return $user->esPropietario($reconocimiento,$reconocimiento->estudiante_id);
 
-    }
+   /**
+    * Determine whether the user can create models.
+    */
+   public function create(User $user): bool
+   {
+       return true;
 
-    public function validar(User $user, Reconocimiento $reconocimiento)
-    {
-        return $user->esDocente();
-    }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Reconocimiento $reconocimiento): bool
-    {
-        //
-    }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Reconocimiento $reconocimiento): bool
-    {
-        //
-    }
+
+   }
+
+
+   /**
+    * Determine whether the user can update the model.
+    */
+   public function update(User $user, Reconocimiento $reconocimiento): bool
+   {
+       return $user->esPropietario($reconocimiento,$reconocimiento->estudiante_id);
+   }
+
+
+   /**
+    * Determine whether the user can delete the model.
+    */
+   public function delete(User $user, Reconocimiento $reconocimiento): bool
+   {
+       return $user->esPropietario($reconocimiento,$reconocimiento->estudiante_id);
+
+
+   }
+
+
+   /**
+    * Determine whether the user can restore the model.
+    */
+   public function restore(User $user, Reconocimiento $reconocimiento): bool
+   {
+       //
+   }
+
+
+   /**
+    * Determine whether the user can permanently delete the model.
+    */
+   public function forceDelete(User $user, Reconocimiento $reconocimiento): bool
+   {
+       //
+   }
 }
+
+
