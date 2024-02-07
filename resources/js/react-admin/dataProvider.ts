c483b7347aa696e5be25e00358785635d8051359
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-02-07 12:42:10 +0100

    El recurso devuelve attachements sin modificar el valor de fichero
    Teniendo en cuenta el caso en el que se haya modificado el registro sin
    modificar el fichero.

--- resources/js/react-admin/dataProvider.ts
+++ resources/js/react-admin/dataProvider.ts
@@ -72,7 +72,7 @@ dataProvider.postLogout = () => {
 };
 
 dataProvider.update = (resource, params) => {
-    if (resource !== 'proyectos' || !params.data.fichero) {
+    if (resource !== 'proyectos' || !params.data.attachments) {
         return originalDataProvider.update(resource, params);
     }
 
@@ -81,7 +81,7 @@ dataProvider.update = (resource, params) => {
         formData.append(`${property}`, `${params.data[property]}`);
     }
 
-    formData.append('fichero', params.data.fichero.rawFile)
+    formData.append('fichero', params.data.attachments.rawFile)
     formData.append('_method', 'PUT')
 
     const url = `${apiUrl}/${resource}/${params.id}`
