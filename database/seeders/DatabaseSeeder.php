81cd27bc581dc19626c0e2ece43ff05f4eb1e60a
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-02-20 08:57:49 +0100

    No crear empresas inicialmente. Las crearán los docentes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,7 +34,6 @@ class DatabaseSeeder extends Seeder
         $this->call(Users_ciclosTableSeeder::class);
         $this->call(CompetenciasTableSeeder::class);
         $this->call(UsersCompetenciasTableSeeder::class);
-        $this->call(EmpresasTableSeeder::class);
         $this->call(IdiomasTableSeeder::class);
         $this->call(UsersIdiomasTableSeeder::class);
 
