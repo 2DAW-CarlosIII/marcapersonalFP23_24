edb8ab28bc8bcd39acee5de6bbaeae9a431c60f1
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-04-08 19:31:13 +0200

    Utilizando los settings para el link del logo Contando los estudiantes en lugar de todos los users

--- resources/js/Pages/front/src/componentes/LogoCabecera/LogoCabecera.jsx
+++ resources/js/Pages/front/src/componentes/LogoCabecera/LogoCabecera.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import './LogoCabecera.css'
 import Logo from '../../assets/img/mp-logoRoja.png';
+import { appUrl } from "../../settings";
 
 const LogoCabecera = () => {
 
     return(
       <ul className="nav navbar-nav">
-            <li><a href={ import.meta.env.VITE_PUSHER_HOST + "/" }><img src={Logo} width="90" height="60" alt="Logo Marca Personal"></img><span className="sr-only">(current)</span></a></li>
+            <li><a href={ appUrl + "/" }><img src={Logo} width="90" height="60" alt="Logo Marca Personal"></img><span className="sr-only">(current)</span></a></li>
      </ul>
       
     )
